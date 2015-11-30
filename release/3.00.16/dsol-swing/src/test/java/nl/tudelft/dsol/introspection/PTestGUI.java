71ee4c35b79b4f9ab8b98fd6600681a815ee04f3
averbraeck <averbraeck@f713b660-dc99-47c1-a074-784ff2779db6>
2015-11-30 22:56:36 +0000

    Release 3.00.16

---
+++ release/3.00.16/dsol-swing/src/test/java/nl/tudelft/dsol/introspection/PTestGUI.java
@@ -0,0 +1,45 @@
+
+package nl.tudelft.dsol.introspection;
+
+import nl.tudelft.dsol.introspection.beans.GUIBean;
+import nl.tudelft.simulation.introspection.beans.BeanIntrospector;
+import nl.tudelft.simulation.introspection.fields.FieldIntrospector;
+import nl.tudelft.simulation.introspection.gui.IntroSpectionDialog;
+import nl.tudelft.simulation.introspection.gui.ObjectTableModel;
+
+/**
+ * A test program for the GUI package.
+ * <p>
+ * (c) copyright 2002-2005-2004 <a href="http://www.simulation.tudelft.nl">Delft University of Technology </a>, the
+ * Netherlands. <br>
+ * See for project information <a href="http://www.simulation.tudelft.nl">www.simulation.tudelft.nl </a> <br>
+ * License of use: <a href="http://www.gnu.org/copyleft/lesser.html">Lesser General Public License (LGPL) </a>, no
+ * warranty.
+ * @author <a href="http://web.eur.nl/fbk/dep/dep1/Introduction/Staff/People/Lang">Niels Lang </a><a
+ *         href="https://www.linkedin.com/in/peterhmjacobs">Peter Jacobs </a>
+ * @version 1.1 Apr 15, 2004
+ * @since 1.5
+ */
+public final class PTestGUI
+{
+    /**
+     * constructs a new PTestGUI.
+     */
+    private PTestGUI()
+    {
+        super();
+        // unreachable code
+    }
+
+    /**
+     * executes the PTestSorter
+     * @param args the command-line arguments
+     */
+    public static void main(final String[] args)
+    {
+        Object introspected = new GUIBean();
+        new IntroSpectionDialog("Test IntrospectionField introspector", new ObjectTableModel(introspected,
+                new FieldIntrospector()));
+        new IntroSpectionDialog("Test Bean introspector", new ObjectTableModel(introspected, new BeanIntrospector()));
+    }
+}
