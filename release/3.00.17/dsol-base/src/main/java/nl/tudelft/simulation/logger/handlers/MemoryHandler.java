4e5b88ea086a22ab7c839340d420661f2aa6cf35
averbraeck <averbraeck@f713b660-dc99-47c1-a074-784ff2779db6>
2015-12-13 19:59:56 +0000

    Release 3.00.17.

---
+++ release/3.00.17/dsol-base/src/main/java/nl/tudelft/simulation/logger/handlers/MemoryHandler.java
@@ -0,0 +1,115 @@
+package nl.tudelft.simulation.logger.handlers;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.logging.Formatter;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+
+/**
+ * A MemoryHandler which makes it possible to push to a dynamic target. <br>
+ * (c) copyright 2002-2005 <a href="http://www.simulation.tudelft.nl">Delft University of Technology </a>, the
+ * Netherlands. <br>
+ * See for project information <a href="http://www.simulation.tudelft.nl"> www.simulation.tudelft.nl </a> <br>
+ * License of use: <a href="http://www.gnu.org/copyleft/lesser.html">Lesser General Public License (LGPL) </a>, no
+ * warranty.
+ * @version $Revision: 1.2 $ $Date: 2010/08/10 11:39:18 $
+ * @author Peter Jacobs, Niels Lang, Alexander Verbraeck
+ */
+public class MemoryHandler extends Handler
+{
+    /** the size of the memoryHandler. */
+    private int size = 100;
+
+    /** the entires of the handler. */
+    private List<LogRecord> entries = Collections.synchronizedList(new ArrayList<LogRecord>(100));
+
+    /**
+     * constructs a new MemoryHandler.
+     */
+    public MemoryHandler()
+    {
+        super();
+    }
+
+    /**
+     * constructs a new MemoryHandler.
+     * @param size the size of the memoryHandler
+     */
+    public MemoryHandler(final int size)
+    {
+        super();
+        this.size = size;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void close()
+    {
+        this.flush();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void flush()
+    {
+        this.entries.clear();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public Formatter getFormatter()
+    {
+        return null;
+    }
+
+    /**
+     * returns the size
+     * @return int the number of records this handler can hold
+     */
+    public int getSize()
+    {
+        return this.size;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public synchronized void publish(final LogRecord logRecord)
+    {
+        this.entries.add(logRecord);
+        if (this.entries.size() > this.size)
+        {
+            this.entries.subList(0, this.entries.size() - this.size).clear();
+        }
+    }
+
+    /**
+     * pushes the memory to a handler
+     * @param target the target
+     */
+    public synchronized void push(final Handler target)
+    {
+        for (LogRecord record : this.entries)
+        {
+            target.publish(record);
+        }
+        this.flush();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void setFormatter(final Formatter arg0)
+    {
+        throw new IllegalArgumentException("Cannot set a " + arg0 + " on this handler");
+    }
+
+    /**
+     * sets the number of records this handler can hold
+     * @param size the size
+     */
+    public void setSize(final int size)
+    {
+        this.size = size;
+    }
+}
---
+++ release/3.00.17/dsol-base/src/test/java/nl/tudelft/simulation/event/EventProducerChild.java
@@ -0,0 +1,38 @@
+package nl.tudelft.simulation.event;
+
+import java.io.Serializable;
+
+/**
+ * The EventProducerChild is an event producer used in JUNIT tests.
+ * <p>
+ * (c) copyright 2002-2005-2004 <a href="http://www.simulation.tudelft.nl">Delft University of Technology </a>, the
+ * Netherlands. <br>
+ * See for project information <a href="http://www.simulation.tudelft.nl">www.simulation.tudelft.nl </a> <br>
+ * License of use: <a href="http://www.gnu.org/copyleft/lesser.html">Lesser General Public License (LGPL) </a>, no
+ * warranty.
+ * @author <a href="https://www.linkedin.com/in/peterhmjacobs">Peter Jacobs </a>
+ * @version $Revision: 1.2 $ $Date: 2010/08/10 11:38:11 $
+ * @since 1.5
+ */
+public class EventProducerChild extends EventProducer implements Serializable
+{
+    /** The default serial version UID for serializable classes. */
+    private static final long serialVersionUID = 1L;
+
+    /** EVENT_A is merely a test event. */
+    public static final EventType EVENT_A = new EventType("EVENT_A");
+
+    /** EVENT_B is merely a test event. */
+    public static final EventType EVENT_B = new EventType("EVENT_B");
+
+    /** EVENT_C is merely a test event. */
+    public static final EventType EVENT_C = new EventType("EVENT_C");
+
+    /**
+     * constructs a new EventProducerChild.
+     */
+    protected EventProducerChild()
+    {
+        super();
+    }
+}
