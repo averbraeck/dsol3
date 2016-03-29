c7f129c3e6068f5392913149c0e4810db26593f4
averbraeck <averbraeck@f713b660-dc99-47c1-a074-784ff2779db6>
2016-03-29 01:20:12 +0000

    DSOL 3.00.19 Added the method 'runUpToAndIncluding' in the DEVSSimulatorInterface and implemented the method in all classes that implement this interface. In the dsol-swing project, changed the zoom with the scroll wheel to scroll around the cursor. The zoom with the plus and minus keys still zoom around the center position of the window. x and y positions of the mouse in world coordinates are shown with a tooltip. The tooltip can be switched on and off. x and y positions of the mouse in world coordinates can be requested with a method to show elsewhere on the control panel of a simulation. In the dsol-swing project, ensured in the introspection package that no errors will be given when the source has been deleted. In the dsol-swing project, created a method to zoom to the entire animation, and a public callable method to zoom to the whole extent as well as to set the default extent. In the dsol-swing project, fixed a small error in computeVisibleExtent(...) that caused extent zoom actions to be off sometimes.

---
+++ release/3.00.19/dsol-base/src/main/java/nl/tudelft/simulation/dsol/interpreter/process/PROCESSINVOKESPECIAL.java
@@ -0,0 +1,75 @@
+package nl.tudelft.simulation.dsol.interpreter.process;
+
+import java.io.DataInput;
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
+import nl.tudelft.simulation.dsol.interpreter.Frame;
+import nl.tudelft.simulation.dsol.interpreter.Interpreter;
+import nl.tudelft.simulation.dsol.interpreter.operations.custom.CUSTOMINVOKESPECIAL;
+import nl.tudelft.simulation.dsol.interpreter.operations.custom.InterpreterOracleInterface;
+import nl.tudelft.simulation.language.concurrent.Monitor;
+
+/**
+ * PROCESSINVOKESPECIAL.
+ * <p>
+ * (c) copyright 2002-2014 <a href="http://www.simulation.tudelft.nl">Delft University of Technology</a>. <br>
+ * BSD-style license. See <a href="http://www.simulation.tudelft.nl/dsol/3.0/license.html">DSOL License</a>. <br>
+ * @author <a href="https://www.linkedin.com/in/peterhmjacobs">Peter Jacobs</a>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ */
+public class PROCESSINVOKESPECIAL extends CUSTOMINVOKESPECIAL
+{
+    /**
+     * constructs a new PROCESSINVOKEVIRTUAL.
+     * @param interpreterOracle the oracle
+     * @param dataInput the dataInput
+     * @throws IOException on IOfailure
+     */
+    public PROCESSINVOKESPECIAL(final InterpreterOracleInterface interpreterOracle, final DataInput dataInput)
+            throws IOException
+    {
+        super(interpreterOracle, dataInput);
+    }
+
+    /**
+     * executes the operation and returns a new Frame.
+     * @param frame the original frame
+     * @param objectRef the object on which to invoke the method
+     * @param arguments the arguments with which to invoke the method
+     * @param method the method to invoke
+     * @throws Exception on invocation exception
+     * @return a new frame
+     */
+    @Override
+    public final Frame execute(final Frame frame, final Object objectRef, final Method method, final Object[] arguments)
+            throws Exception
+    {
+        if (!InterpretableProcess.class.isAssignableFrom(method.getDeclaringClass()))
+        {
+            return super.execute(frame, objectRef, method, arguments);
+        }
+        if (method.getName().equals("suspend"))
+        {
+            return super.execute(frame, objectRef, method, arguments);
+        }
+
+        // Let's check for the suspend method
+        if (method.equals(ProcessFactory.suspendMethod))
+        {
+            // we set the state of the process to suspended
+            InterpretableProcess process = (InterpretableProcess) objectRef;
+            process.setState(InterpretableProcess.SUSPENDED);
+
+            // we pause the frame
+            frame.setPaused(true);
+            return frame;
+        }
+        if (Modifier.isSynchronized(method.getModifiers()))
+        {
+            Monitor.lock(objectRef);
+        }
+        return Interpreter.createFrame(objectRef, method, arguments);
+    }
+}
---
+++ release/3.00.19/dsol-base/src/main/java/nl/tudelft/simulation/logger/handlers/EventLogHandler.java
@@ -0,0 +1,126 @@
+/*
+ * @(#) EventLogHandler.java Nov 18, 2003 Copyright (c) 2002-2005 Delft
+ * University of Technology Jaffalaan 5, 2628 BX Delft, the Netherlands. All
+ * rights reserved. This software is proprietary information of Delft University
+ * of Technology 
+ */
+package nl.tudelft.simulation.logger.handlers;
+
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+
+import nl.tudelft.simulation.event.Event;
+import nl.tudelft.simulation.event.EventInterface;
+import nl.tudelft.simulation.event.EventListenerInterface;
+import nl.tudelft.simulation.event.EventProducer;
+import nl.tudelft.simulation.event.EventProducerInterface;
+import nl.tudelft.simulation.event.EventType;
+
+/**
+ * A EventLogHandler <br>
+ * (c) copyright 2002-2005 <a href="http://www.simulation.tudelft.nl">Delft University of Technology </a>, the
+ * Netherlands. <br>
+ * See for project information <a href="http://www.simulation.tudelft.nl">www.simulation.tudelft.nl </a> <br>
+ * License of use: <a href="http://www.gnu.org/copyleft/lesser.html">Lesser General Public License (LGPL) </a>, no
+ * warranty.
+ * @version $Revision: 1.2 $ $Date: 2010/08/10 11:39:18 $
+ * @author Peter Jacobs, Niels Lang, Alexander Verbraeck
+ */
+public class EventLogHandler extends Handler implements EventProducerInterface
+{
+    /** LOG_RECORD_PRODUCED_EVENT is fired whenever an log record is received. */
+    public static final EventType LOG_RECORD_PRODUCED_EVENT = new EventType("LOG_RECORD_PRODUCED_EVENT");
+
+    /** our private postman */
+    private MyEventProducer postman = new MyEventProducer();
+
+    /**
+     * constructs a new EventLogHandler
+     */
+    public EventLogHandler()
+    {
+        super();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public boolean addListener(final EventListenerInterface listener, final EventType eventType)
+    {
+        return this.postman.addListener(listener, eventType);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public boolean addListener(final EventListenerInterface listener, final EventType eventType, final boolean weak)
+    {
+        return this.postman.addListener(listener, eventType, weak);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public boolean addListener(final EventListenerInterface listener, final EventType eventType, final short position)
+    {
+        return this.postman.addListener(listener, eventType, position);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public boolean addListener(final EventListenerInterface listener, final EventType eventType, final short position,
+            final boolean weak)
+    {
+        return this.postman.addListener(listener, eventType, position, weak);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void close()
+    {
+        this.flush();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void flush()
+    {
+        // Nothing to do
+    }
+
+    /**
+     * @return the event types. Not a method of EventProducerInterface anymore. TODO see if the method can be removed.
+     */
+    public EventType[] getEventTypes()
+    {
+        return new EventType[]{EventLogHandler.LOG_RECORD_PRODUCED_EVENT};
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void publish(final LogRecord arg0)
+    {
+        this.postman.fireEvent(new Event(EventLogHandler.LOG_RECORD_PRODUCED_EVENT, this, this.getFormatter().format(
+                arg0)));
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public boolean removeListener(final EventListenerInterface listener, final EventType eventType)
+    {
+        return this.postman.removeListener(listener, eventType);
+    }
+
+    /**
+     * A MyEventProducer is a more or less public Postman.
+     */
+    protected class MyEventProducer extends EventProducer
+    {
+        /** The default serial version UID for serializable classes */
+        private static final long serialVersionUID = 1L;
+
+        /** {@inheritDoc} */
+        @Override
+        public EventInterface fireEvent(final EventInterface event)
+        {
+            return super.fireEvent(event);
+        }
+    }
+}
