005df755dca19804a8a132a6715b31fdbf608142
averbraeck <averbraeck@f713b660-dc99-47c1-a074-784ff2779db6>
2016-03-29 01:21:01 +0000

    DSOL 3.00.19 Added the method 'runUpToAndIncluding' in the DEVSSimulatorInterface and implemented the method in all classes that implement this interface. In the dsol-swing project, changed the zoom with the scroll wheel to scroll around the cursor. The zoom with the plus and minus keys still zoom around the center position of the window. x and y positions of the mouse in world coordinates are shown with a tooltip. The tooltip can be switched on and off. x and y positions of the mouse in world coordinates can be requested with a method to show elsewhere on the control panel of a simulation. In the dsol-swing project, ensured in the introspection package that no errors will be given when the source has been deleted. In the dsol-swing project, created a method to zoom to the entire animation, and a public callable method to zoom to the whole extent as well as to set the default extent. In the dsol-swing project, fixed a small error in computeVisibleExtent(...) that caused extent zoom actions to be off sometimes.

---
+++ release/3.00.19/dsol-core/src/main/java/nl/tudelft/simulation/dsol/simtime/UnitTime.java
@@ -0,0 +1,174 @@
+package nl.tudelft.simulation.dsol.simtime;
+
+import java.io.Serializable;
+
+/**
+ * <p>
+ * Copyright (c) 2002-2014 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights
+ * reserved.
+ * <p>
+ * See for project information <a href="http://www.simulation.tudelft.nl/"> www.simulation.tudelft.nl</a>.
+ * <p>
+ * The DSOL project is distributed under the following BSD-style license:<br>
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+ * following conditions are met:
+ * <ul>
+ * <li>Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+ * disclaimer.</li>
+ * <li>Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
+ * following disclaimer in the documentation and/or other materials provided with the distribution.</li>
+ * <li>Neither the name of Delft University of Technology, nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.</li>
+ * </ul>
+ * This software is provided by the copyright holders and contributors "as is" and any express or implied warranties,
+ * including, but not limited to, the implied warranties of merchantability and fitness for a particular purpose are
+ * disclaimed. In no event shall the copyright holder or contributors be liable for any direct, indirect, incidental,
+ * special, exemplary, or consequential damages (including, but not limited to, procurement of substitute goods or
+ * services; loss of use, data, or profits; or business interruption) however caused and on any theory of liability,
+ * whether in contract, strict liability, or tort (including negligence or otherwise) arising in any way out of the use
+ * of this software, even if advised of the possibility of such damage.
+ * @version Aug 3, 2014 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @param <N> the number type to use to indicate time, such as Double, Float or Long.
+ * @param <T> the class itself to force implementation of 'Comparable'.
+ */
+public abstract class UnitTime<N extends Number, T extends UnitTime<N, T>> extends Number implements Comparable<T>,
+        Serializable
+{
+    /** */
+    private static final long serialVersionUID = 20140802L;
+
+    /** value represents the value in milliseconds. */
+    @SuppressWarnings("checkstyle:visibilitymodifier")
+    protected N timeMsec;
+
+    /** original unit. */
+    @SuppressWarnings("checkstyle:visibilitymodifier")
+    protected final TimeUnit unit;
+
+    /**
+     * @param time in units.
+     * @param unit the unit, e.g. HOUR.
+     */
+    public UnitTime(final N time, final TimeUnit unit)
+    {
+        this.timeMsec = convertToMsec(time, unit);
+        this.unit = unit;
+    }
+
+    /**
+     * @return the time in milliseconds
+     */
+    public final N getTimeMsec()
+    {
+        return this.timeMsec;
+    }
+
+    /**
+     * @param value the new time in milliseconds.
+     */
+    public final void setTimeMsec(final N value)
+    {
+        this.timeMsec = value;
+    }
+
+    /**
+     * @param time in units.
+     * @param timeUnit the unit, e.g. HOUR.
+     * @return the equivalent of this time in milliseconds, using the required umber type.
+     */
+    protected abstract N convertToMsec(final N time, final TimeUnit timeUnit);
+
+    /**
+     * @return the time in units.
+     */
+    public abstract N getTime();
+
+    /**
+     * @return the unit
+     */
+    public final TimeUnit getUnit()
+    {
+        return this.unit;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final int intValue()
+    {
+        return this.timeMsec.intValue();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final long longValue()
+    {
+        return this.timeMsec.longValue();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final float floatValue()
+    {
+        return this.timeMsec.floatValue();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final double doubleValue()
+    {
+        return this.timeMsec.doubleValue();
+    }
+
+    /**
+     * converts this unit-time to the target units.
+     * @param targetUnits the units to convert to
+     * @return the amount in target units
+     */
+    public final double convert(final TimeUnit targetUnits)
+    {
+        return this.timeMsec.doubleValue() * this.unit.getFactor() / targetUnits.getFactor();
+    }
+
+    /**
+     * @return HH:MM:SS
+     */
+    public final String formatHMS()
+    {
+        double hours = getHours();
+        int days = (int) Math.floor(hours / 24.0);
+        double currentHour = hours - 24.0 * days;
+        int h = (int) Math.floor(currentHour);
+        int m = (int) (((long) Math.floor(hours * 60.0)) % 60);
+        int s = (int) (((long) Math.floor(hours * 3600.0)) % 60);
+        return String.format("%02d:%02d:%02d", h, m, s);
+    }
+
+    /**
+     * @return HHH:MM:SS
+     */
+    public final String formatHHHMS()
+    {
+        double hours = getHours();
+        int h = (int) Math.floor(hours);
+        int m = (int) (((long) Math.floor(hours * 60.0)) % 60);
+        int s = (int) (((long) Math.floor(hours * 3600.0)) % 60);
+        return String.format("%02d:%02d:%02d", h, m, s);
+    }
+
+    /**
+     * @return number of hours in SimTime
+     */
+    public final double getHours()
+    {
+        return convert(TimeUnit.HOUR);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    @SuppressWarnings("checkstyle:designforextension")
+    public String toString()
+    {
+        return convert(this.unit) + this.unit.getAbbreviation();
+    }
+}
---
+++ release/3.00.19/dsol-core/src/test/java/nl/tudelft/simulation/dsol/DSOLTestSuite.java
@@ -0,0 +1,46 @@
+package nl.tudelft.simulation.dsol;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import nl.tudelft.simulation.dsol.eventList.EventListTest;
+import nl.tudelft.simulation.dsol.experiment.TimeUnitTest;
+import nl.tudelft.simulation.dsol.serialize.SerializeTest;
+
+/**
+ * The DSOL TestSuite defines the JUnit Test Suite which tests all DSOL classes. <br>
+ * (c) copyright 2002-2005 <a href="http://www.simulation.tudelft.nl"> Delft University of Technology </a>, the
+ * Netherlands. <br>
+ * See for project information <a href="http://www.simulation.tudelft.nl"> www.simulation.tudelft.nl </a> <br>
+ * License of use: <a href="http://www.gnu.org/copyleft/lesser.html">Lesser General Public License (LGPL) </a>, no
+ * warranty.
+ * @version 2.0 21.09.2003 <br>
+ * @author <a href="https://www.linkedin.com/in/peterhmjacobs">Peter Jacobs </a>, <a
+ *         href="mailto:a.verbraeck@tudelft.nl">Alexander Verbraeck </a>
+ */
+public final class DSOLTestSuite
+{
+    /**
+     * constructs a new DSOLTestSuite.
+     */
+    private DSOLTestSuite()
+    {
+        super();
+    }
+
+    /**
+     * constructs the test suite
+     * @return Test the main DSOL Test Suite
+     */
+    public static Test suite()
+    {
+        TestSuite suite = new TestSuite("DSOL Test Suite");
+        suite.addTest(new EventListTest());
+        suite.addTest(new TimeUnitTest());
+        suite.addTest(new SerializeTest("SerializeTest"));
+        // suite.addTest(new DESSSimulatorTest());
+        // suite.addTest(new DEVSSimulatorTest());
+        // suite.addTest(new RealTimeClockTest_Failed());
+        // TODO suite.addTest(new SimulatorTest());
+        return suite;
+    }
+}
