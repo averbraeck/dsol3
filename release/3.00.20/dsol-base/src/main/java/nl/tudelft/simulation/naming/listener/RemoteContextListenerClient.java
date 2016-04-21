490abf8193eff61da78582ad22087f07d7e52f64
averbraeck <averbraeck@f713b660-dc99-47c1-a074-784ff2779db6>
2016-04-21 14:05:56 +0000

    DSOL 3.00.20 release.

---
+++ release/3.00.20/dsol-base/src/main/java/nl/tudelft/simulation/naming/listener/RemoteContextListenerClient.java
@@ -0,0 +1,106 @@
+package nl.tudelft.simulation.naming.listener;
+
+import java.rmi.RemoteException;
+
+import javax.naming.event.NamingEvent;
+import javax.naming.event.NamingExceptionEvent;
+
+import nl.tudelft.simulation.logger.Logger;
+
+/**
+ * The local wrapper for remoteContextListeners.
+ * <p>
+ * (c) copyright 2002-2005 <a href="http://www.simulation.tudelft.nl">Delft University of Technology </a>, the
+ * Netherlands. <br>
+ * See for project information <a href="http://www.simulation.tudelft.nl">www.simulation.tudelft.nl </a> <br>
+ * License of use: <a href="http://www.gnu.org/copyleft/lesser.html">Lesser General Public License (LGPL) </a>, no
+ * warranty.
+ * @author <a href="https://www.linkedin.com/in/peterhmjacobs">Peter Jacobs </a>
+ * @version 1.2 Apr 14, 2004
+ * @since 1.5
+ */
+public class RemoteContextListenerClient implements ContextListenerInterface
+{
+    /** the target to use. */
+    private RemoteContextListenerInterface target = null;
+
+    /**
+     * constructs a new RemoteContextListenerClient
+     * @param target the remote target on which method invocation must be tunneled.
+     */
+    public RemoteContextListenerClient(final RemoteContextListenerInterface target)
+    {
+        super();
+        this.target = target;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void namingExceptionThrown(NamingExceptionEvent evt)
+    {
+        try
+        {
+            this.target.namingExceptionThrown(evt);
+        }
+        catch (RemoteException remoteException)
+        {
+            Logger.warning(this, "objectChanged", remoteException);
+        }
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void objectAdded(NamingEvent evt)
+    {
+        try
+        {
+            this.target.objectAdded(evt);
+        }
+        catch (RemoteException remoteException)
+        {
+            Logger.warning(this, "objectAdded", remoteException);
+        }
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void objectRemoved(NamingEvent evt)
+    {
+        try
+        {
+            this.target.objectRemoved(evt);
+        }
+        catch (RemoteException remoteException)
+        {
+            Logger.warning(this, "objectRemoved", remoteException);
+        }
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void objectRenamed(NamingEvent evt)
+    {
+        try
+        {
+            this.target.objectRenamed(evt);
+        }
+        catch (RemoteException remoteException)
+        {
+            Logger.warning(this, "objectRenamed", remoteException);
+        }
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void objectChanged(NamingEvent evt)
+    {
+        try
+        {
+            this.target.objectChanged(evt);
+        }
+        catch (RemoteException remoteException)
+        {
+            Logger.warning(this, "objectChanged", remoteException);
+        }
+    }
+}
