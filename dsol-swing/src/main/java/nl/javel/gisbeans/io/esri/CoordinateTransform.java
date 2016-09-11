9dc74b4d149d13fbd9c75ce96fd7663876c25369
Alexander Verbraeck <averbraeck@users.noreply.github.com>
2016-09-11 23:47:04 +0000

    Repaired malformed javadoc comments.

--- dsol-swing/src/main/java/nl/javel/gisbeans/io/esri/CoordinateTransform.java
+++ dsol-swing/src/main/java/nl/javel/gisbeans/io/esri/CoordinateTransform.java
@@ -2,7 +2,7 @@ package nl.javel.gisbeans.io.esri;
 
 /**
  * Transforms an (x, y) coordinate to a new (x', y') coordinate
- * <p> </p>
+ * <br>
  * (c) copyright 2002-2014 <a href="http://www.simulation.tudelft.nl">Delft University of Technology</a>. <br>
  * BSD-style license. See <a href="http://www.simulation.tudelft.nl/dsol/3.0/license.html">DSOL License</a>. <br>
  * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
@@ -28,7 +28,7 @@ public interface CoordinateTransform
     
     /**
      * The identical transformation (x,y) => (x,y).
-     * <p> </p>
+     * <br>
      * (c) copyright 2002-2014 <a href="http://www.simulation.tudelft.nl">Delft University of Technology</a>. <br>
      * BSD-style license. See <a href="http://www.simulation.tudelft.nl/dsol/3.0/license.html">DSOL License</a>. <br>
      * @author <a href="https://www.linkedin.com/in/peterhmjacobs">Peter Jacobs</a>
