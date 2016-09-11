49d874f2c09df1c4155084c5732603d35d78a0cf
averbraeck <averbraeck@f713b660-dc99-47c1-a074-784ff2779db6>
2016-09-11 23:39:14 +0000

    Repaired malformed javadoc comments.

--- trunk/dsol-base/src/test/java/nl/tudelft/dsol/introspection/FieldIntrospectionBeansTest.java
+++ trunk/dsol-base/src/test/java/nl/tudelft/dsol/introspection/FieldIntrospectionBeansTest.java
@@ -10,7 +10,7 @@ import nl.tudelft.simulation.logger.Logger;
  * @author (c) 2003 <a href="http://www.tudelft.nl">Delft University of Technology </a>, Delft, the Netherlands <br>
  *         <a href="http://www.tbm.tudelft.nl">Faculty of Technology, Policy and Management </a> <br>
  *         <a href="http://www.sk.tbm.tudelft.nl">Department of System Engineering </a> <br>
- *         Main researcher : <a href="http://www.tbm.tudelft.nl/webstaf/alexandv/">Dr. Ir. A. Verbraeck <a/><br>
+ *         Main researcher : <a href="http://www.tbm.tudelft.nl/webstaf/alexandv/">Dr. Ir. A. Verbraeck</a><br>
  *         Assistant researchers <a href="https://www.linkedin.com/in/peterhmjacobs">Ir. P.H.M. Jacobs </a> and <a
  *         href="http://www.tbm.tudelft.nl/webstaf/nielsl">Ir. N.A. Lang </a>
  */
