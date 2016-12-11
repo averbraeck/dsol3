e13c822f6bd93eeb2133c20b0c2611284e888a28
averbraeck <averbraeck@f713b660-dc99-47c1-a074-784ff2779db6>
2016-12-11 16:42:30 +0000

    Deployed DSOL version 3.01.02

---
+++ release/3.01.02/dsol-core/src/main/java/nl/tudelft/simulation/dsol/simtime/SimTimeCalendarLong.java
@@ -0,0 +1,130 @@
+package nl.tudelft.simulation.dsol.simtime;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+/**
+ * <p>
+ * Copyright (c) 2002-2014 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights
+ * reserved.
+ * <p>
+ * See for project information <a href="http://www.simulation.tudelft.nl/"> www.simulation.tudelft.nl</a>.
+ * <p>
+ * The DSOL project is distributed under the following BSD-style license:<br>
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+ * following conditions are met:
+ * <ul>
+ * <li>Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+ * disclaimer.</li>
+ * <li>Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
+ * following disclaimer in the documentation and/or other materials provided with the distribution.</li>
+ * <li>Neither the name of Delft University of Technology, nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.</li>
+ * </ul>
+ * This software is provided by the copyright holders and contributors "as is" and any express or implied warranties,
+ * including, but not limited to, the implied warranties of merchantability and fitness for a particular purpose are
+ * disclaimed. In no event shall the copyright holder or contributors be liable for any direct, indirect, incidental,
+ * special, exemplary, or consequential damages (including, but not limited to, procurement of substitute goods or
+ * services; loss of use, data, or profits; or business interruption) however caused and on any theory of liability,
+ * whether in contract, strict liability, or tort (including negligence or otherwise) arising in any way out of the use
+ * of this software, even if advised of the possibility of such damage.
+ * @version Jul 25, 2014 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ */
+public class SimTimeCalendarLong extends SimTime<Calendar, UnitTimeLong, SimTimeCalendarLong>
+{
+    /** */
+    private static final long serialVersionUID = 1L;
+
+    /** value represents the value in milliseconds. */
+    private long timeMsec;
+
+    /**
+     * Constructor. super(time) calls set(time).
+     * @param time the calendar time item.
+     */
+    public SimTimeCalendarLong(final Calendar time)
+    {
+        super(time);
+    }
+
+    /**
+     * Constructor based on (long) millisecond time.
+     * @param timeMsec the calendar time in milliseconds.
+     */
+    public SimTimeCalendarLong(final long timeMsec)
+    {
+        super(new GregorianCalendar());
+        this.timeMsec = timeMsec;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final void add(final UnitTimeLong relativeTime)
+    {
+        this.timeMsec += relativeTime.getTimeMsec();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final void subtract(final UnitTimeLong relativeTime)
+    {
+        this.timeMsec -= relativeTime.getTimeMsec();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final UnitTimeLong minus(final SimTimeCalendarLong simTime)
+    {
+        UnitTimeLong ret = new UnitTimeLong(this.timeMsec, TimeUnit.MILLISECOND);
+        ret.setTimeMsec(ret.getTimeMsec() - simTime.timeMsec);
+        return ret;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final int compareTo(final SimTimeCalendarLong simTime)
+    {
+        return Long.compare(this.timeMsec, simTime.timeMsec);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final SimTimeCalendarLong setZero()
+    {
+        this.timeMsec = 0L;
+        return this;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final SimTimeCalendarLong copy()
+    {
+        return new SimTimeCalendarLong(get());
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final void set(final Calendar absoluteTime)
+    {
+        this.timeMsec = absoluteTime.getTimeInMillis();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final Calendar get()
+    {
+        Calendar cal = new GregorianCalendar();
+        cal.setTimeInMillis(this.timeMsec);
+        return cal;
+    }
+
+    /**
+     * @return timeMsec
+     */
+    public final long getTimeMsec()
+    {
+        return this.timeMsec;
+    }
+
+}
---
+++ release/3.01.02/dsol-core/src/main/java/nl/tudelft/simulation/jstats/distributions/DistDiscreteEmpirical.java
@@ -0,0 +1,148 @@
+package nl.tudelft.simulation.jstats.distributions;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+import nl.tudelft.simulation.jstats.distributions.empirical.Observations;
+import nl.tudelft.simulation.jstats.distributions.empirical.ObservationsInterface;
+import nl.tudelft.simulation.jstats.streams.StreamInterface;
+
+/**
+ * The discrete empirical distribution as defined on page 326 of Law &amp; Kelton.
+ * <p>
+ * (c) copyright 2002-2004 <a href="http://www.simulation.tudelft.nl">Delft University of Technology </a>, the
+ * Netherlands. <br>
+ * See for project information <a href="http://www.simulation.tudelft.nl"> www.simulation.tudelft.nl </a> <br>
+ * License of use: <a href="http://www.gnu.org/copyleft/lesser.html">Lesser General Public License (LGPL) </a>, no
+ * warranty.
+ * @author <a href="https://www.linkedin.com/in/peterhmjacobs"> Peter Jacobs </a>
+ * @version $Revision: 1.2 $ $Date: 2010/08/10 11:38:39 $
+ * @since 1.5
+ */
+public class DistDiscreteEmpirical extends DistDiscrete
+{
+    /** the observations. */
+    private ObservationsInterface observations = null;
+
+    /**
+     * constructs a new DistDiscreteEmpirical distribution.
+     * @param stream the stream to use
+     * @param observations the observations feeding the distributions. These observations should be in one of two
+     *            possible formats. The first accepted format is the X(i);probability format. This results in a map with
+     *            values such as 1=0.33;2=0.167;3=0.167;4=0.33. The second allowed format is the X(i);occurrence
+     *            combination resulting in values such as 1=2;2=1;3=1;4=2.
+     */
+    public DistDiscreteEmpirical(final StreamInterface stream, final ObservationsInterface observations)
+    {
+        super(stream);
+        if (!observations.isGrouped())
+        {
+            this.observations =
+                    new Observations(DistDiscreteEmpirical.constructGroupedMap(observations.getObservations()), false);
+        }
+        else
+        {
+            this.observations = observations;
+        }
+    }
+
+    /**
+     * constructs a new DistDiscreteEmpirical distribution.
+     * @param stream the stream to use
+     * @param observations the observations feeding the distributions. This sortedmap should be filled with observation
+     *            probability values. The probability may either reflect the number of times this observation is
+     *            observed or may contain a relative probability.
+     */
+    public DistDiscreteEmpirical(final StreamInterface stream, final Long[] observations)
+    {
+        super(stream);
+        this.observations =
+                new Observations(DistDiscreteEmpirical.constructGroupedMap(Arrays.asList(observations)), false);
+    }
+
+    /**
+     * constructs a new DistDiscreteEmpirical distribution.
+     * @param stream the stream to use
+     * @param observations the observations feeding the distributions. This sortedmap should be filled with observation
+     *            probability values. The probability may either reflect the number of times this observation is
+     *            observed or may contain a relative probability.
+     */
+    public DistDiscreteEmpirical(final StreamInterface stream, final long[] observations)
+    {
+        super(stream);
+        Long[] values = new Long[observations.length];
+        for (int i = 0; i < values.length; i++)
+        {
+            values[i] = new Long(observations[i]);
+        }
+        this.observations = new Observations(DistDiscreteEmpirical.constructGroupedMap(Arrays.asList(values)), false);
+    }
+
+    /**
+     * constructs a new DistDiscreteEmpirical distribution.
+     * @param stream the stream to use
+     * @param observations the observations feeding the distributions. This sortedmap should be filled with observation
+     *            probability values. The probability may either reflect the number of times this observation is
+     *            observed or may contain a relative probability.
+     * @param cummulative reflects whether the probabilities are cummulative
+     */
+    public DistDiscreteEmpirical(final StreamInterface stream, final SortedMap<Number, Double> observations,
+            final boolean cummulative)
+    {
+        super(stream);
+        this.observations = new Observations(observations, cummulative);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public long draw()
+    {
+        double u = super.stream.nextDouble();
+        return this.observations.getCeilingEntry(new Double(u), ObservationsInterface.CUMPROBABILITY, true)
+                .getObservation().longValue();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public double probability(final int observation)
+    {
+        if (this.observations.contains(new Long(observation), ObservationsInterface.OBSERVATION))
+        {
+            int index = this.observations
+                    .getIndex(this.observations.getEntry(new Long(observation), ObservationsInterface.OBSERVATION));
+            if (index > 0)
+            {
+                return this.observations.get(index).getCumProbability().doubleValue()
+                        - this.observations.get(index - 1).getCumProbability().doubleValue();
+            }
+            return this.observations.get(index).getCumProbability().doubleValue();
+        }
+        return 0.0;
+    }
+
+    /**
+     * constructs a grouped map since we do not have the draw and probability specification for the non-grouped discrete
+     * empirical distribution.
+     * @param observations the non grouped empirical distribution
+     * @return a new SortedMap which is not normalized and not cummulative.
+     */
+    private static SortedMap<Number, Double> constructGroupedMap(final List<? extends Number> observations)
+    {
+        SortedMap<Number, Double> result = new TreeMap<Number, Double>();
+        for (Number entry : observations)
+        {
+            if (result.containsKey(entry))
+            {
+                Number value = result.get(entry);
+                result.put(entry, value.doubleValue() + 1);
+            }
+            else
+            {
+                result.put(entry, 1.0);
+            }
+        }
+        return result;
+    }
+}
