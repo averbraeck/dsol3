e8aedca1a09e2ec9711c0c433f9d4cc1e9db9bc1
averbraeck <averbraeck@f713b660-dc99-47c1-a074-784ff2779db6>
2017-01-14 22:52:58 +0000

    deployed version 3.01.03

---
+++ release/3.01.03/dsol-interpreter/src/main/java/nl/tudelft/simulation/dsol/interpreter/operations/ASTORE_1.java
@@ -0,0 +1,51 @@
+package nl.tudelft.simulation.dsol.interpreter.operations;
+
+import nl.tudelft.simulation.dsol.interpreter.LocalVariable;
+import nl.tudelft.simulation.dsol.interpreter.OperandStack;
+import nl.tudelft.simulation.dsol.interpreter.classfile.Constant;
+
+/**
+ * The ASTORE_1 operation as defined in <a
+ * href="http://java.sun.com/docs/books/vmspec/2nd-edition/html/Instructions2.doc.html">
+ * http://java.sun.com/docs/books/vmspec/2nd-edition/html/Instructions2.doc.html </a>.
+ * <p>
+ * (c) copyright 2002-2014 <a href="http://www.simulation.tudelft.nl">Delft University of Technology</a>. <br>
+ * BSD-style license. See <a href="http://www.simulation.tudelft.nl/dsol/3.0/license.html">DSOL License</a>. <br>
+ * @author <a href="https://www.linkedin.com/in/peterhmjacobs">Peter Jacobs</a>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ */
+public class ASTORE_1 extends VoidOperation
+{
+    /** OP refers to the operand code. */
+    public static final int OP = 76;
+
+    /**
+     * constructs a new ASTORE_1.
+     */
+    public ASTORE_1()
+    {
+        super();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final void execute(final OperandStack stack, final Constant[] constantPool,
+            final LocalVariable[] localVariables)
+    {
+        localVariables[1].setValue(stack.pop());
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final int getByteLength()
+    {
+        return OPCODE_BYTE_LENGTH;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final int getOpcode()
+    {
+        return ASTORE_1.OP;
+    }
+}
