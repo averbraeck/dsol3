9845baee22148c1ce8c736ce40ef2e4a36fd294e
averbraeck <averbraeck@f713b660-dc99-47c1-a074-784ff2779db6>
2017-01-14 22:53:19 +0000

    deployed version 3.01.03

---
+++ release/3.01.03/dsol-swing/src/main/java/nl/javel/gisbeans/io/esri/CoordinateTransform.java
@@ -0,0 +1,56 @@
+package nl.javel.gisbeans.io.esri;
+
+/**
+ * Transforms an (x, y) coordinate to a new (x', y') coordinate
+ * <br>
+ * (c) copyright 2002-2014 <a href="http://www.simulation.tudelft.nl">Delft University of Technology</a>. <br>
+ * BSD-style license. See <a href="http://www.simulation.tudelft.nl/dsol/3.0/license.html">DSOL License</a>. <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @version Nov 30, 2015
+ */
+public interface CoordinateTransform
+{
+    /**
+     * transform the (x, y) coordinate to a new (x', y') coordinate represented as a float[2].
+     * @param x the original x-coordinate, e.g. lon in dd (double degrees)
+     * @param y the original y-coordinate, e.g. lat in dd (double degrees)
+     * @return the new (x', y') coordinate represented as a float[2]
+     */
+    float[] floatTransform(double x, double y);
+
+    /**
+     * transform the (x, y) coordinate to a new (x', y') coordinate represented as a double[2].
+     * @param x the original x-coordinate, e.g. lon in dd (double degrees)
+     * @param y the original y-coordinate, e.g. lat in dd (double degrees)
+     * @return the new (x', y') coordinate represented as a double[2]
+     */
+    double[] doubleTransform(double x, double y);
+    
+    /**
+     * The identical transformation (x,y) =&gt; (x,y).
+     * <br>
+     * (c) copyright 2002-2014 <a href="http://www.simulation.tudelft.nl">Delft University of Technology</a>. <br>
+     * BSD-style license. See <a href="http://www.simulation.tudelft.nl/dsol/3.0/license.html">DSOL License</a>. <br>
+     * @author <a href="https://www.linkedin.com/in/peterhmjacobs">Peter Jacobs</a>
+     * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+     * @version Nov 30, 2015
+     */
+    class NoTransform implements CoordinateTransform
+    {
+
+        /** {@inheritDoc} */
+        @Override
+        public final float[] floatTransform(final double x, final double y)
+        {
+            return new float[]{(float) x, (float) y};
+        }
+
+        /** {@inheritDoc} */
+        @Override
+        public final double[] doubleTransform(final double x, final double y)
+        {
+            return new double[]{x, y};
+        }
+        
+    }
+}
---
+++ release/3.01.03/dsol-swing/src/main/java/nl/tudelft/simulation/introspection/mapping/MyDefaultEditor.java
@@ -0,0 +1,88 @@
+package nl.tudelft.simulation.introspection.mapping;
+
+import java.awt.Component;
+import java.util.EventObject;
+
+import javax.swing.DefaultCellEditor;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.event.CellEditorListener;
+import javax.swing.table.TableCellEditor;
+
+/**
+ * The default editor.
+ * <p>
+ * (c) copyright 2002-2014 <a href="http://www.simulation.tudelft.nl">Delft University of Technology</a>. <br>
+ * BSD-style license. See <a href="http://www.simulation.tudelft.nl/dsol/3.0/license.html">DSOL License</a>. <br>
+ * @author <a href="https://www.linkedin.com/in/peterhmjacobs">Peter Jacobs</a>.
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>.
+ * @author Niels Lang.
+ * @since 1.5
+ */
+public class MyDefaultEditor implements TableCellEditor
+{
+    /** the delegate. */
+    private TableCellEditor delegate = new DefaultCellEditor(new JTextField());
+
+    /** {@inheritDoc} */
+    @Override
+    public Component getTableCellEditorComponent(final JTable table, final Object value, final boolean isSelected,
+            final int row, final int column)
+    {
+        TableCellEditor editor = table.getDefaultEditor(value.getClass());
+        if (!(editor instanceof MyDefaultEditor))
+        {
+            this.delegate = editor;
+        }
+        return this.delegate.getTableCellEditorComponent(table, value, isSelected, row, column);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void cancelCellEditing()
+    {
+        this.delegate.cancelCellEditing();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public Object getCellEditorValue()
+    {
+        return this.delegate.getCellEditorValue();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public boolean isCellEditable(final EventObject anEvent)
+    {
+        return this.delegate.isCellEditable(anEvent);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public boolean shouldSelectCell(final EventObject anEvent)
+    {
+        return this.delegate.shouldSelectCell(anEvent);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public boolean stopCellEditing()
+    {
+        return this.delegate.stopCellEditing();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void addCellEditorListener(final CellEditorListener l)
+    {
+        this.delegate.addCellEditorListener(l);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void removeCellEditorListener(final CellEditorListener l)
+    {
+        this.delegate.removeCellEditorListener(l);
+    }
+}
