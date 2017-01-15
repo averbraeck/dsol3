21bc64b64adabd6b0245165d7493a3cc75a65d4e
averbraeck <averbraeck@f713b660-dc99-47c1-a074-784ff2779db6>
2017-01-15 03:08:35 +0000

    deployed version 3.01.03

---
+++ release/3.01.03/dsol-core/src/main/java/nl/tudelft/simulation/jstats/streams/Java2Random.java
@@ -0,0 +1,108 @@
+package nl.tudelft.simulation.jstats.streams;
+
+import java.util.Random;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.XStreamException;
+
+/**
+ * The Java2Random is an extention of the <code>java.util.Random</code> class which implements the StreamInterface.
+ * <p>
+ * (c) copyright 2004 <a href="http://www.simulation.tudelft.nl">Delft University of Technology </a>, the Netherlands.
+ * <br>
+ * See for project information <a href="http://www.simulation.tudelft.nl"> www.simulation.tudelft.nl </a> <br>
+ * License of use: <a href="http://www.gnu.org/copyleft/lesser.html">Lesser General Public License (LGPL) </a>, no
+ * warranty.
+ * @author <a href="https://www.linkedin.com/in/peterhmjacobs">Peter Jacobs </a>
+ * @version $Revision: 1.2 $ $Date: 2010/08/10 11:38:40 $
+ * @since 1.5
+ */
+public class Java2Random extends Random implements StreamInterface
+{
+    /** */
+    private static final long serialVersionUID = 20140831L;
+
+    /**
+     * seed is a link to the seed value. The reason to store the seed in this variable is that there is no getSeed() on
+     * the Java2Random
+     */
+    private long seed;
+
+    /**
+     * creates a new Java2Random and in initializes with System.currentTimeMillis constructs a new Java2Random.
+     */
+    public Java2Random()
+    {
+        this(System.currentTimeMillis());
+    }
+
+    /**
+     * reates a new Java2Random and in initializes with a given seed.
+     * @param seed the seed to use.
+     */
+    public Java2Random(final long seed)
+    {
+        super(seed);
+        this.seed = seed;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final void reset()
+    {
+        this.setSeed(this.seed);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final int nextInt(final int i, final int j)
+    {
+        return i + (int) Math.floor((j - i + 1) * this.nextDouble());
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final synchronized void setSeed(final long seed)
+    {
+        this.seed = seed;
+        super.setSeed(seed);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final long getSeed()
+    {
+        return this.seed;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final Object saveState() throws StreamException
+    {
+        XStream xstream = new XStream();
+        try
+        {
+            return xstream.toXML(this);
+        }
+        catch (XStreamException exception)
+        {
+            throw new StreamException(exception);
+        }
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final void restoreState(final Object state) throws StreamException
+    {
+        XStream xstream = new XStream();
+        try
+        {
+            xstream.fromXML((String) state, this);
+        }
+        catch (XStreamException exception)
+        {
+            throw new StreamException(exception);
+        }
+    }
+
+}
