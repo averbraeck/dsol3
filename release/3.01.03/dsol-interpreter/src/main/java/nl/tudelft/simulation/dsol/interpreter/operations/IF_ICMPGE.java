2e05dacbb3e882f16ac6639511a459a5780e816e
averbraeck <averbraeck@f713b660-dc99-47c1-a074-784ff2779db6>
2017-01-15 03:08:29 +0000

    deployed version 3.01.03

---
+++ release/3.01.03/dsol-interpreter/src/main/java/nl/tudelft/simulation/dsol/interpreter/operations/IF_ICMPGE.java
@@ -0,0 +1,67 @@
+package nl.tudelft.simulation.dsol.interpreter.operations;
+
+import java.io.DataInput;
+import java.io.IOException;
+
+import nl.tudelft.simulation.dsol.interpreter.LocalVariable;
+import nl.tudelft.simulation.dsol.interpreter.OperandStack;
+import nl.tudelft.simulation.dsol.interpreter.classfile.Constant;
+import nl.tudelft.simulation.language.primitives.Primitive;
+
+/**
+ * The IF_ICMPGE operation as defined in <a
+ * href="http://java.sun.com/docs/books/vmspec/2nd-edition/html/Instructions2.doc6.html">
+ * http://java.sun.com/docs/books/vmspec/2nd-edition/html/Instructions2.doc6.html </a>.
+ * <p>
+ * (c) copyright 2002-2014 <a href="http://www.simulation.tudelft.nl">Delft University of Technology</a>. <br>
+ * BSD-style license. See <a href="http://www.simulation.tudelft.nl/dsol/3.0/license.html">DSOL License</a>. <br>
+ * @author <a href="https://www.linkedin.com/in/peterhmjacobs">Peter Jacobs</a>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ */
+public class IF_ICMPGE extends JumpOperation
+{
+    /** OP refers to the operand code. */
+    public static final int OP = 162;
+
+    /** the index to load. */
+    private final int offset;
+
+    /**
+     * constructs a new IF_ICMPGE.
+     * @param dataInput the dataInput
+     * @throws IOException on IOfailure
+     */
+    public IF_ICMPGE(final DataInput dataInput) throws IOException
+    {
+        super();
+        this.offset = dataInput.readShort();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final int execute(final OperandStack stack, final Constant[] constantPool,
+            final LocalVariable[] localVariables)
+    {
+        Integer obj2 = Primitive.toInteger(stack.pop());
+        Integer obj1 = Primitive.toInteger(stack.pop());
+        if (obj1.compareTo(obj2) >= 0)
+        {
+            return this.offset;
+        }
+        return this.getByteLength();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final int getByteLength()
+    {
+        return OPCODE_BYTE_LENGTH + 2;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final int getOpcode()
+    {
+        return IF_ICMPGE.OP;
+    }
+}
