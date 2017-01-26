c768d02a217ab1003aadd3769b0a458e2dc34b25
averbraeck <averbraeck@f713b660-dc99-47c1-a074-784ff2779db6>
2017-01-26 23:10:28 +0000

    deployed version 3.01.04

---
+++ release/3.01.04/dsol-core/src/main/java/nl/tudelft/simulation/dsol/simulators/Simulator.java
@@ -0,0 +1,360 @@
+package nl.tudelft.simulation.dsol.simulators;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.rmi.RemoteException;
+import java.util.Calendar;
+
+import nl.tudelft.simulation.dsol.SimRuntimeException;
+import nl.tudelft.simulation.dsol.experiment.Replication;
+import nl.tudelft.simulation.dsol.experiment.ReplicationMode;
+import nl.tudelft.simulation.dsol.simtime.SimTime;
+import nl.tudelft.simulation.dsol.simtime.SimTimeCalendarDouble;
+import nl.tudelft.simulation.dsol.simtime.SimTimeCalendarFloat;
+import nl.tudelft.simulation.dsol.simtime.SimTimeCalendarLong;
+import nl.tudelft.simulation.dsol.simtime.SimTimeDouble;
+import nl.tudelft.simulation.dsol.simtime.SimTimeDoubleUnit;
+import nl.tudelft.simulation.dsol.simtime.SimTimeFloat;
+import nl.tudelft.simulation.dsol.simtime.SimTimeFloatUnit;
+import nl.tudelft.simulation.dsol.simtime.SimTimeLong;
+import nl.tudelft.simulation.dsol.simtime.SimTimeLongUnit;
+import nl.tudelft.simulation.dsol.simtime.UnitTimeDouble;
+import nl.tudelft.simulation.dsol.simtime.UnitTimeFloat;
+import nl.tudelft.simulation.dsol.simtime.UnitTimeLong;
+import nl.tudelft.simulation.event.EventProducer;
+import nl.tudelft.simulation.jstats.statistics.StatisticsObject;
+import nl.tudelft.simulation.language.concurrent.WorkerThread;
+
+/**
+ * The Simulator class is an abstract implementation of the SimulatorInterface.
+ * <p>
+ * (c) copyright 2002-2005 <a href="http://www.simulation.tudelft.nl">Delft University of Technology </a>, the
+ * Netherlands. <br>
+ * See for project information <a href="http://www.simulation.tudelft.nl"> www.simulation.tudelft.nl </a> <br>
+ * License of use: <a href="http://www.gnu.org/copyleft/lesser.html">Lesser General Public License (LGPL) </a>, no
+ * warranty.
+ * @author <a href="https://www.linkedin.com/in/peterhmjacobs">Peter Jacobs </a>
+ * @version $Revision: 1.2 $ $Date: 2010/08/10 11:36:44 $
+ * @param <A> the absolute storage type for the simulation time, e.g. Calendar, UnitTimeDouble, or Double.
+ * @param <R> the relative type for time storage, e.g. Long for the Calendar. For most non-calendar types, the absolute
+ *            and relative types are the same.
+ * @param <T> the extended type itself to be able to implement a comparator on the simulation time.
+ * @since 1.5
+ */
+public abstract class Simulator<A extends Comparable<A>, R extends Number & Comparable<R>, T extends SimTime<A, R, T>>
+        extends EventProducer implements SimulatorInterface<A, R, T>, Runnable
+{
+    /** */
+    private static final long serialVersionUID = 20140805L;
+
+    /** simulatorTime represents the simulationTime. */
+    @SuppressWarnings("checkstyle:visibilitymodifier")
+    protected T simulatorTime;
+
+    /** running represents the binary state of the simulator. */
+    @SuppressWarnings("checkstyle:visibilitymodifier")
+    protected transient boolean running = false;
+
+    /** replication represents the currently active replication. */
+    @SuppressWarnings("checkstyle:visibilitymodifier")
+    protected Replication<A, R, T> replication = null;
+
+    /** a worker. */
+    @SuppressWarnings("checkstyle:visibilitymodifier")
+    protected transient WorkerThread worker = null;
+
+    /** the simulatorSemaphore. */
+    @SuppressWarnings("checkstyle:visibilitymodifier")
+    protected transient Object semaphore = new Object();
+
+    /**
+     * constructs a new Simulator.
+     */
+    public Simulator()
+    {
+        super();
+        this.worker = new WorkerThread(this.getClass().getName(), this);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final T getSimulatorTime()
+    {
+        return this.simulatorTime;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final Replication<A, R, T> getReplication()
+    {
+        return this.replication;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    @SuppressWarnings("checkstyle:designforextension")
+    public void initialize(final Replication<A, R, T> initReplication, final ReplicationMode replicationMode)
+            throws RemoteException, SimRuntimeException
+    {
+        if (initReplication == null)
+        {
+            throw new IllegalArgumentException("replication == null ?");
+        }
+        if (this.isRunning())
+        {
+            throw new SimRuntimeException("Cannot initialize a running simulator");
+        }
+        synchronized (this.semaphore)
+        {
+            this.removeAllListeners(StatisticsObject.class);
+            this.replication = initReplication;
+            this.simulatorTime = initReplication.getTreatment().getStartTime().copy();
+            this.fireTimedEvent(SimulatorInterface.START_REPLICATION_EVENT, this.simulatorTime, this.simulatorTime.get());
+            this.fireTimedEvent(SimulatorInterface.TIME_CHANGED_EVENT, this.simulatorTime, this.simulatorTime.get());
+        }
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final boolean isRunning()
+    {
+        return this.running;
+    }
+
+    /**
+     * The run method defines the actual time step mechanism of the simulator. The implementation of this method depends
+     * on the formalism. Where discrete event formalisms loop over an eventlist continuous simulators take pre-defined
+     * time steps.
+     */
+    @Override
+    public abstract void run();
+
+    /** {@inheritDoc} */
+    @Override
+    @SuppressWarnings("checkstyle:designforextension")
+    public void start(final boolean fireStartEvent) throws SimRuntimeException
+    {
+        if (this.isRunning())
+        {
+            throw new SimRuntimeException("Cannot start a running simulator");
+        }
+        if (this.replication == null)
+        {
+            throw new SimRuntimeException("Cannot start a simulator" + " without replication details");
+        }
+        if (this.simulatorTime.ge(this.replication.getTreatment().getEndTime()))
+        {
+            throw new SimRuntimeException("Cannot start simulator : " + "simulatorTime = runLength");
+        }
+        synchronized (this.semaphore)
+        {
+            this.running = true;
+            if (fireStartEvent)
+            {
+                this.fireEvent(START_EVENT);
+            }
+            this.fireTimedEvent(SimulatorInterface.TIME_CHANGED_EVENT, this.simulatorTime, this.simulatorTime.get());
+            if (!Thread.currentThread().getName().equals(this.worker.getName()))
+            {
+                this.worker.interrupt();
+            }
+            else
+            {
+                this.run();
+            }
+        }
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    @SuppressWarnings("checkstyle:designforextension")
+    public final void start() throws SimRuntimeException
+    {
+        start(true);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    @SuppressWarnings("checkstyle:designforextension")
+    public void step(final boolean fireStepEvent) throws SimRuntimeException
+    {
+        if (this.isRunning())
+        {
+            throw new SimRuntimeException("Cannot step a running simulator");
+        }
+        if (this.replication == null)
+        {
+            throw new SimRuntimeException("Cannot step a simulator " + "without replication details");
+        }
+        if (this.simulatorTime.ge(this.replication.getTreatment().getEndTime()))
+        {
+            throw new SimRuntimeException("Cannot step simulator: " + "SimulatorTime = runControl.runLength");
+        }
+        if (fireStepEvent)
+        {
+            this.fireEvent(SimulatorInterface.STEP_EVENT);
+        }
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    @SuppressWarnings("checkstyle:designforextension")
+    public final void step() throws SimRuntimeException
+    {
+        step(true);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    @SuppressWarnings("checkstyle:designforextension")
+    public void stop(final boolean fireStopEvent)
+    {
+        if (this.isRunning())
+        {
+            this.running = false;
+            if (this.simulatorTime.ge(this.getReplication().getTreatment().getEndTime()))
+            {
+                this.fireTimedEvent(SimulatorInterface.END_OF_REPLICATION_EVENT, this, this.simulatorTime.get());
+            }
+            if (fireStopEvent)
+            {
+                this.fireEvent(SimulatorInterface.STOP_EVENT);
+            }
+        }
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    @SuppressWarnings("checkstyle:designforextension")
+    public final void stop()
+    {
+        stop(true);
+    }
+
+    /**
+     * Clean up the simulator. Remove the worker thread.
+     */
+    public final void cleanUp()
+    {
+        this.running = false;
+        if (this.listeners != null)
+        {
+            this.removeAllListeners();
+        }
+        if (this.worker != null)
+        {
+            this.worker.cleanUp();
+        }
+        this.worker = null;
+    }
+
+    /**
+     * writes a serializable method to stream.
+     * @param out the outputstream
+     * @throws IOException on IOException
+     */
+    private synchronized void writeObject(final ObjectOutputStream out) throws IOException
+    {
+        out.writeObject(this.simulatorTime);
+        out.writeObject(this.replication);
+    }
+
+    /**
+     * reads a serializable method from stream.
+     * @param in the inputstream
+     * @throws IOException on IOException
+     */
+    @SuppressWarnings("unchecked")
+    private synchronized void readObject(final java.io.ObjectInputStream in) throws IOException
+    {
+        try
+        {
+            this.simulatorTime = (T) in.readObject();
+            this.replication = (Replication<A, R, T>) in.readObject();
+            this.running = false;
+            this.semaphore = new Object();
+            this.worker = new WorkerThread(this.getClass().getName(), this);
+        }
+        catch (Exception exception)
+        {
+            throw new IOException(exception.getMessage());
+        }
+    }
+
+    /***********************************************************************************************************/
+    /************************************* EASY ACCESS CLASS EXTENSIONS ****************************************/
+    /***********************************************************************************************************/
+
+    /** Easy access class Simulator.TimeDouble. */
+    public abstract static class TimeDouble extends Simulator<Double, Double, SimTimeDouble>
+            implements SimulatorInterface.TimeDouble
+    {
+        /** */
+        private static final long serialVersionUID = 20140805L;
+    }
+
+    /** Easy access class Simulator.TimeFloat. */
+    public abstract static class TimeFloat extends Simulator<Float, Float, SimTimeFloat>
+            implements SimulatorInterface.TimeFloat
+    {
+        /** */
+        private static final long serialVersionUID = 20140805L;
+    }
+
+    /** Easy access class Simulator.TimeLong. */
+    public abstract static class TimeLong extends Simulator<Long, Long, SimTimeLong>
+            implements SimulatorInterface.TimeLong
+    {
+        /** */
+        private static final long serialVersionUID = 20140805L;
+    }
+
+    /** Easy access class Simulator.TimeDoubleUnit. */
+    public abstract static class TimeDoubleUnit extends Simulator<UnitTimeDouble, UnitTimeDouble, SimTimeDoubleUnit>
+            implements SimulatorInterface.TimeDoubleUnit
+    {
+        /** */
+        private static final long serialVersionUID = 20140805L;
+    }
+
+    /** Easy access class Simulator.TimeFloatUnit. */
+    public abstract static class TimeFloatUnit extends Simulator<UnitTimeFloat, UnitTimeFloat, SimTimeFloatUnit>
+            implements SimulatorInterface.TimeFloatUnit
+    {
+        /** */
+        private static final long serialVersionUID = 20140805L;
+    }
+
+    /** Easy access class Simulator.TimeLongUnit. */
+    public abstract static class TimeLongUnit extends Simulator<UnitTimeLong, UnitTimeLong, SimTimeLongUnit>
+            implements SimulatorInterface.TimeLongUnit
+    {
+        /** */
+        private static final long serialVersionUID = 20140805L;
+    }
+
+    /** Easy access class Simulator.CalendarDouble. */
+    public abstract static class CalendarDouble extends Simulator<Calendar, UnitTimeDouble, SimTimeCalendarDouble>
+            implements SimulatorInterface.CalendarDouble
+    {
+        /** */
+        private static final long serialVersionUID = 20140805L;
+    }
+
+    /** Easy access class Simulator.CalendarFloat. */
+    public abstract static class CalendarFloat extends Simulator<Calendar, UnitTimeFloat, SimTimeCalendarFloat>
+            implements SimulatorInterface.CalendarFloat
+    {
+        /** */
+        private static final long serialVersionUID = 20140805L;
+    }
+
+    /** Easy access class Simulator.CalendarLong. */
+    public abstract static class CalendarLong extends Simulator<Calendar, UnitTimeLong, SimTimeCalendarLong>
+            implements SimulatorInterface.CalendarLong
+    {
+        /** */
+        private static final long serialVersionUID = 20140805L;
+    }
+
+}
---
+++ release/3.01.04/dsol-core/src/test/java/nl/tudelft/simulation/dsol/simulators/TestModel.java
@@ -0,0 +1,87 @@
+package nl.tudelft.simulation.dsol.simulators;
+
+import java.rmi.RemoteException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import nl.tudelft.simulation.dsol.DSOLModel;
+import nl.tudelft.simulation.event.EventInterface;
+import nl.tudelft.simulation.event.EventListenerInterface;
+
+/**
+ * The TestModel <br>
+ * (c) copyright 2002-2005 <a href="http://www.simulation.tudelft.nl">Delft University of Technology </a>, the
+ * Netherlands. <br>
+ * See for project information <a href="http://www.simulation.tudelft.nl">www.simulation.tudelft.nl </a> <br>
+ * License of use: <a href="http://www.gnu.org/copyleft/lesser.html">Lesser General Public License (LGPL) </a>, no
+ * warranty.
+ * @version 2.0 21.09.2003 <br>
+ * @author <a href="https://www.linkedin.com/in/peterhmjacobs">Peter Jacobs </a>,
+ *         <a href="mailto:a.verbraeck@tudelft.nl">Alexander Verbraeck </a>
+ */
+public class TestModel implements DSOLModel, EventListenerInterface
+{
+
+    /** the startTime. */
+    private long startTime = 0L;
+
+    /** the simulator. */
+    private SimulatorInterface simulator;
+    
+    /** the logger. */
+    private static Logger logger = LogManager.getLogger(TestModel.class);
+
+    /**
+     * constructs a new TestModel.
+     */
+    public TestModel()
+    {
+        super();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void constructModel(final SimulatorInterface simulator) throws RemoteException
+    {
+        this.simulator = simulator;
+        simulator.addListener(this, SimulatorInterface.END_OF_REPLICATION_EVENT, false);
+        simulator.addListener(this, SimulatorInterface.START_REPLICATION_EVENT, false);
+        simulator.addListener(this, SimulatorInterface.START_EVENT, false);
+        simulator.addListener(this, SimulatorInterface.STOP_EVENT, false);
+        simulator.addListener(this, SimulatorInterface.STEP_EVENT, false);
+        simulator.addListener(this, SimulatorInterface.TIME_CHANGED_EVENT, false);
+    }
+
+    /**
+     * @return the simulator
+     */
+    public SimulatorInterface getSimulator()
+    {
+        return this.simulator;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void notify(final EventInterface event)
+    {
+        if (event.getType().equals(SimulatorInterface.START_EVENT))
+        {
+            this.startTime = System.currentTimeMillis();
+            System.out.println("started @ " + this.startTime);
+        }
+        if (event.getType().equals(SimulatorInterface.STOP_EVENT))
+        {
+            long runLength = System.currentTimeMillis() - this.startTime;
+            try
+            {
+                System.out.println("runlength=" + runLength + " time="
+                        + ((SimulatorInterface) event.getSource()).getSimulatorTime());
+            }
+            catch (RemoteException exception)
+            {
+                logger.warn("notify", exception);
+            }
+        }
+    }
+}
