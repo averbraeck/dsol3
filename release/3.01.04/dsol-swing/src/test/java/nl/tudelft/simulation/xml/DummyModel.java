5e9652fd426de5f2dcde15d518b55cee7c0b667b
averbraeck <averbraeck@f713b660-dc99-47c1-a074-784ff2779db6>
2017-01-26 23:10:42 +0000

    deployed version 3.01.04

---
+++ release/3.01.04/dsol-swing/src/test/java/nl/tudelft/simulation/xml/DummyModel.java
@@ -0,0 +1,41 @@
+package nl.tudelft.simulation.xml;
+
+import java.rmi.RemoteException;
+
+import nl.tudelft.simulation.dsol.DSOLModel;
+import nl.tudelft.simulation.dsol.simulators.SimulatorInterface;
+
+/**
+ * A DummyTestModel
+ * <p>
+ * (c) copyright 2002-2005 <a href="http://www.simulation.tudelft.nl">Delft University of Technology </a>, the
+ * Netherlands. <br>
+ * See for project information <a href="http://www.simulation.tudelft.nl">www.simulation.tudelft.nl </a> <br>
+ * License of use: <a href="http://www.gnu.org/copyleft/lesser.html">Lesser General Public License (LGPL) </a>, no
+ * warranty.
+ * @author <a href="https://www.linkedin.com/in/peterhmjacobs">Peter Jacobs </a>
+ * @version 1.2 Sep 28, 2004
+ * @since 1.5
+ */
+public class DummyModel implements DSOLModel
+{
+    /**
+     * constructs a new DummyModel.
+     */
+    public DummyModel()
+    {
+        super();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void constructModel(final SimulatorInterface simulator)
+    {
+        // We have constructed a model.
+    }
+
+    public SimulatorInterface getSimulator() throws RemoteException
+    {
+        return null;
+    }
+}
