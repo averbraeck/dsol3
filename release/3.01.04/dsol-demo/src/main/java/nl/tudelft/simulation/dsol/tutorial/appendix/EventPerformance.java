3294a709f5908dae96d544ffa2cfb9dc40694e88
averbraeck <averbraeck@f713b660-dc99-47c1-a074-784ff2779db6>
2017-01-26 19:24:43 +0000

    deployed version 3.01.04

---
+++ release/3.01.04/dsol-demo/src/main/java/nl/tudelft/simulation/dsol/tutorial/appendix/EventPerformance.java
@@ -0,0 +1,105 @@
+package nl.tudelft.simulation.dsol.tutorial.appendix;
+
+import java.rmi.RemoteException;
+
+import nl.tudelft.simulation.dsol.DSOLModel;
+import nl.tudelft.simulation.dsol.SimRuntimeException;
+import nl.tudelft.simulation.dsol.experiment.ExperimentalFrame;
+import nl.tudelft.simulation.dsol.simulators.DEVSSimulatorInterface;
+import nl.tudelft.simulation.dsol.simulators.SimulatorInterface;
+import nl.tudelft.simulation.language.io.URLResource;
+import nl.tudelft.simulation.xml.dsol.ExperimentParser;
+
+/**
+ * A EventPerformance <br>
+ * (c) copyright 2002-2016 <a href="http://www.simulation.tudelft.nl"> Delft University of Technology </a>, the
+ * Netherlands. <br>
+ * See for project information <a href="http://www.simulation.tudelft.nl"> www.simulation.tudelft.nl </a> <br>
+ * License of use: <a href="http://www.gnu.org/copyleft/lesser.html">Lesser General Public License (LGPL) </a>, no
+ * warranty.
+ * @version 1.0 Jan 19, 2004 <br>
+ * @author <a href="https://www.linkedin.com/in/peterhmjacobs">Peter Jacobs </a>
+ */
+public class EventPerformance implements DSOLModel
+{
+    /** The default serial version UID for serializable classes. */
+    private static final long serialVersionUID = 1L;
+
+    /** the simulator. */
+    private SimulatorInterface.TimeDouble simulator;
+
+    /**
+     * constructs a new EventPerformance.
+     */
+    public EventPerformance()
+    {
+        super();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void constructModel(final SimulatorInterface.TimeDouble simulator) throws SimRuntimeException, RemoteException
+    {
+        this.simulator = simulator;
+        DEVSSimulatorInterface.TimeDouble devsSimulator = (DEVSSimulatorInterface) simulator;
+        devsSimulator.scheduleEvent(10000, this, this, "complete", new Object[]{new Long(System.currentTimeMillis())});
+        for (int i = 0; i < 100000; i++)
+        {
+            devsSimulator.scheduleEvent(10000, this, this, "stepA", new Object[]{devsSimulator});
+        }
+    }
+
+    /**
+     * @return the simulator
+     */
+    public SimulatorInterface.TimeDouble getSimulator()
+    {
+        return this.simulator;
+    }
+
+    /**
+     * stepA
+     * @param simulator
+     * @throws SimRuntimeException
+     * @throws RemoteException
+     */
+    protected void stepA(final DEVSSimulatorInterface.TimeDouble simulator) throws SimRuntimeException, RemoteException
+    {
+        simulator.scheduleEvent(10, this, this, "stepB", null);
+    }
+
+    /**
+     * stepB
+     */
+    protected void stepB()
+    {
+        // finished
+    }
+
+    /**
+     * completes the run
+     * @param startTime the startTime
+     */
+    protected void complete(long startTime)
+    {
+        System.out.println(System.currentTimeMillis() - startTime);
+    }
+
+    /**
+     * commandline executes the model
+     * @param args the arguments to the commandline
+     */
+    public static void main(final String[] args)
+    {
+        try
+        {
+            ExperimentalFrame experimentalFrame =
+                    ExperimentParser.parseExperimentalFrame(URLResource.getResource("/EventPerformance.xml"));
+            experimentalFrame.start();
+        }
+        catch (Exception exception)
+        {
+            exception.printStackTrace();
+        }
+    }
+}
