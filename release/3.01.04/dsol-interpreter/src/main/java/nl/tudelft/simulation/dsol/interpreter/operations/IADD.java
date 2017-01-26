18b8f670e5fc8655a8a079a0145ad0966ef25c06
averbraeck <averbraeck@f713b660-dc99-47c1-a074-784ff2779db6>
2017-01-26 21:05:54 +0000

    deployed version 3.01.04

---
+++ release/3.01.04/dsol-interpreter/src/main/java/nl/tudelft/simulation/dsol/interpreter/operations/IADD.java
@@ -0,0 +1,54 @@
+package nl.tudelft.simulation.dsol.interpreter.operations;
+
+import nl.tudelft.simulation.dsol.interpreter.LocalVariable;
+import nl.tudelft.simulation.dsol.interpreter.OperandStack;
+import nl.tudelft.simulation.dsol.interpreter.classfile.Constant;
+import nl.tudelft.simulation.language.primitives.Primitive;
+
+/**
+ * The IADD operation as defined in <a
+ * href="http://java.sun.com/docs/books/vmspec/2nd-edition/html/Instructions2.doc6.html">
+ * http://java.sun.com/docs/books/vmspec/2nd-edition/html/Instructions2.doc6.html </a>.
+ * <p>
+ * (c) copyright 2002-2014 <a href="http://www.simulation.tudelft.nl">Delft University of Technology</a>. <br>
+ * BSD-style license. See <a href="http://www.simulation.tudelft.nl/dsol/3.0/license.html">DSOL License</a>. <br>
+ * @author <a href="https://www.linkedin.com/in/peterhmjacobs">Peter Jacobs</a>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ */
+public class IADD extends VoidOperation
+{
+    /** OP refers to the operand code. */
+    public static final int OP = 96;
+
+    /**
+     * constructs a new IADD.
+     */
+    public IADD()
+    {
+        super();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final void execute(final OperandStack stack, final Constant[] constantPool,
+            final LocalVariable[] localVariables)
+    {
+        int i2 = Primitive.toInteger(stack.pop()).intValue();
+        int i1 = Primitive.toInteger(stack.pop()).intValue();
+        stack.push(Integer.valueOf(i1 + i2));
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final int getByteLength()
+    {
+        return OPCODE_BYTE_LENGTH;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final int getOpcode()
+    {
+        return IADD.OP;
+    }
+}
