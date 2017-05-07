1d09fdafabadca5258748b5c69a04e643eee5c19
averbraeck <averbraeck@f713b660-dc99-47c1-a074-784ff2779db6>
2017-05-07 09:22:03 +0000

    DSOL version 3.01.06

---
+++ release/3.01.06/dsol-swing/src/test/java/nl/tudelft/dsol/introspection/beans/GUIBean.java
@@ -0,0 +1,81 @@
+package nl.tudelft.dsol.introspection.beans;
+
+import java.awt.Color;
+import java.awt.Font;
+
+/**
+ * Test bean for testing introspection of simple and composite color and font properties.
+ * <p>
+ * (c) copyright 2002-2005-2004 <a href="http://www.simulation.tudelft.nl">Delft University of Technology </a>, the
+ * Netherlands. <br>
+ * See for project information <a href="http://www.simulation.tudelft.nl">www.simulation.tudelft.nl </a> <br>
+ * License of use: <a href="http://www.gnu.org/copyleft/lesser.html">Lesser General Public License (LGPL) </a>, no
+ * warranty.
+ * @author <a href="http://web.eur.nl/fbk/dep/dep1/Introduction/Staff/People/Lang">Niels Lang
+ *         </a><a href="https://www.linkedin.com/in/peterhmjacobs">Peter Jacobs </a>
+ * @version 1.1 Apr 15, 2004
+ * @since 1.5
+ */
+public class GUIBean
+{
+    /** the color to use. */
+    private Color color = Color.YELLOW;
+
+    /** the font to use. */
+    private Font font = new Font("Arial", Font.BOLD, 11);
+
+    /** the colorSet. */
+    private Color[] colorSet = new Color[]{Color.BLACK, Color.BLUE};
+
+    /**
+     * @return the Color
+     */
+    public Color getColor()
+    {
+        return this.color;
+    }
+
+    /**
+     * @return the Font
+     */
+    public Font getFont()
+    {
+        return this.font;
+    }
+
+    /**
+     * @param color the color of the bean
+     */
+    public void setColor(final Color color)
+    {
+        this.color = color;
+    }
+
+    /**
+     * sets the font
+     * @param font the font
+     */
+    public void setFont(final Font font)
+    {
+        this.font = font;
+    }
+
+    /**
+     * returns the colorSet
+     * @return Color[]
+     */
+    public Color[] getColorSet()
+    {
+        return this.colorSet;
+    }
+
+    /**
+     * sets the colorset.
+     * @param colorSet the colorSet
+     */
+    public void setColorSet(final Color[] colorSet)
+    {
+        this.colorSet = colorSet;
+    }
+
+}
