e9aa4090fbc0aed1511067beae97110294ae1f0d
averbraeck <averbraeck@f713b660-dc99-47c1-a074-784ff2779db6>
2017-05-07 09:21:44 +0000

    DSOL version 3.01.06

---
+++ release/3.01.06/dsol-interpreter/src/main/java/nl/tudelft/simulation/dsol/interpreter/operations/LNEG.java
@@ -0,0 +1,52 @@
+package nl.tudelft.simulation.dsol.interpreter.operations;
+
+import nl.tudelft.simulation.dsol.interpreter.LocalVariable;
+import nl.tudelft.simulation.dsol.interpreter.OperandStack;
+import nl.tudelft.simulation.dsol.interpreter.classfile.Constant;
+
+/**
+ * The LNEG operation as defined in <a
+ * href="http://java.sun.com/docs/books/vmspec/2nd-edition/html/Instructions2.doc8.html">
+ * http://java.sun.com/docs/books/vmspec/2nd-edition/html/Instructions2.doc8.html </a>.
+ * <p>
+ * (c) copyright 2002-2014 <a href="http://www.simulation.tudelft.nl">Delft University of Technology</a>. <br>
+ * BSD-style license. See <a href="http://www.simulation.tudelft.nl/dsol/3.0/license.html">DSOL License</a>. <br>
+ * @author <a href="https://www.linkedin.com/in/peterhmjacobs">Peter Jacobs</a>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ */
+public class LNEG extends VoidOperation
+{
+    /** OP refers to the operand code. */
+    public static final int OP = 117;
+
+    /**
+     * constructs a new LNEG.
+     */
+    public LNEG()
+    {
+        super();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final void execute(final OperandStack stack, final Constant[] constantPool,
+            final LocalVariable[] localVariables)
+    {
+        long l = ((Long) stack.pop()).longValue();
+        stack.push(Long.valueOf(-l));
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final int getByteLength()
+    {
+        return OPCODE_BYTE_LENGTH;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final int getOpcode()
+    {
+        return LNEG.OP;
+    }
+}
