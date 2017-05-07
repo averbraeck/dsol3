4aefdf1e8826d2dd7e9f579a00e3cb67fed99643
averbraeck <averbraeck@f713b660-dc99-47c1-a074-784ff2779db6>
2017-05-07 07:57:50 +0000

    DSOL version 3.01.06

---
+++ release/3.01.06/dsol-demo/src/main/java/nl/tudelft/simulation/dsol/tutorial/section25/Model25.java
@@ -0,0 +1,49 @@
+package nl.tudelft.simulation.dsol.tutorial.section25;
+
+import nl.tudelft.simulation.dsol.DSOLModel;
+import nl.tudelft.simulation.dsol.simtime.SimTimeDouble;
+import nl.tudelft.simulation.dsol.simulators.DEVSSimulatorInterface;
+import nl.tudelft.simulation.dsol.simulators.SimulatorInterface;
+
+/**
+ * The Customer Ordering model class as presented in section 2.5 in the DSOL tutorial.
+ * <p>
+ * (c) copyright 2002-2016 <a href="http://www.simulation.tudelft.nl"> Delft University of Technology </a>, the
+ * Netherlands. <br>
+ * See for project information <a href="http://www.simulation.tudelft.nl"> www.simulation.tudelft.nl </a> <br>
+ * License of use: <a href="http://www.gnu.org/copyleft/lesser.html">Lesser General Public License (LGPL) </a>, no
+ * warranty.
+ * @version 1.1 Sep 6, 2004 <br>
+ * @author <a href="https://www.linkedin.com/in/peterhmjacobs">Peter Jacobs </a>
+ */
+public class Model25 implements DSOLModel.TimeDouble
+{
+    /** The default serial version UID for serializable classes. */
+    private static final long serialVersionUID = 1L;
+
+    /** the simulator. */
+    private SimulatorInterface.TimeDouble simulator;
+
+    /** Construct a new customer ordering model. */
+    public Model25()
+    {
+        super();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final void constructModel(final SimulatorInterface<Double, Double, SimTimeDouble> simulatorDouble)
+    {
+        System.out.println("\nReplication starts...");
+        DEVSSimulatorInterface.TimeDouble devsSimulator = (DEVSSimulatorInterface.TimeDouble) simulatorDouble;
+        this.simulator = devsSimulator;
+        new Customer(devsSimulator);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final SimulatorInterface.TimeDouble getSimulator()
+    {
+        return this.simulator;
+    }
+}
