2bf552483f27866cff46ac360396189c3ec8f165
averbraeck <averbraeck@f713b660-dc99-47c1-a074-784ff2779db6>
2018-09-09 17:05:47 +0000

    version 3.02.01

---
+++ release/3.02.01/dsol-naming/src/main/java/nl/tudelft/simulation/naming/context/ContextTreeModel.java
@@ -0,0 +1,56 @@
+package nl.tudelft.simulation.naming.context;
+
+import javax.naming.NamingException;
+import javax.naming.event.EventContext;
+import javax.swing.tree.DefaultTreeModel;
+
+/**
+ * The ContextTreeModel defines the inner structure of the context.
+ * <p>
+ * (c) 2002-2018 <a href="http://www.simulation.tudelft.nl">Delft University of Technology </a>, the
+ * Netherlands. <br>
+ * See for project information <a href="http://www.simulation.tudelft.nl"> www.simulation.tudelft.nl </a> <br>
+ * License of use: <a href="http://www.gnu.org/copyleft/lesser.html">Lesser General Public License (LGPL) </a>, no
+ * warranty.
+ * @author <a href="https://www.linkedin.com/in/peterhmjacobs">Peter Jacobs </a>
+ * @version 1.2 2004-03-24
+ * @since 1.5
+ */
+public class ContextTreeModel extends DefaultTreeModel
+{
+    /** The default serial version UID for serializable classes. */
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * constructs a new ContextTreeModel.
+     * @param context the context
+     * @throws NamingException on failure
+     */
+    public ContextTreeModel(final EventContext context) throws NamingException
+    {
+        this(context, null, true);
+    }
+
+    /**
+     * constructs a new ContextTreeModel.
+     * @param context the context
+     * @param displayClasses the set of classes to display as children
+     * @param displayFields should we display them?
+     * @throws NamingException on failure
+     */
+    public ContextTreeModel(final EventContext context, final Class<?>[] displayClasses, final boolean displayFields)
+            throws NamingException
+    {
+        super(null);
+        this.setRoot(new ContextNode(this, "/", context, displayClasses, displayFields));
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected void fireTreeStructureChanged(final Object arg0, final Object[] arg1, final int[] arg2,
+            final Object[] arg3)
+    {
+        super.fireTreeStructureChanged(arg0, arg1, arg2, arg3);
+    }
+
+}
---
+++ release/3.02.01/dsol-naming/src/test/java/nl/tudelft/simulation/dsol/naming/ServerTest.java
@@ -0,0 +1,79 @@
+package nl.tudelft.simulation.dsol.naming;
+
+import javax.naming.NamingException;
+import javax.naming.event.EventContext;
+import javax.naming.event.NamingEvent;
+import javax.naming.event.NamingExceptionEvent;
+
+import nl.tudelft.simulation.naming.InitialEventContext;
+import nl.tudelft.simulation.naming.listener.ContextListenerInterface;
+
+
+/**
+ * The ServerTest.
+ * <p>
+ * (c) 2002-2018 <a href="http://www.simulation.tudelft.nl">Delft University of Technology </a>, the
+ * Netherlands. <br>
+ * See for project information <a href="http://www.simulation.tudelft.nl">www.simulation.tudelft.nl </a> <br>
+ * License of use: <a href="http://www.gnu.org/copyleft/lesser.html">Lesser General Public License (LGPL) </a>, no
+ * warranty.
+ * @author <a href="https://www.linkedin.com/in/peterhmjacobs">Peter Jacobs </a>
+ * @version 1.2 Apr 16, 2004
+ * @since 1.5
+ */
+public class ServerTest implements ContextListenerInterface
+{
+    /**
+     * constructs a new ServerTest.
+     * @param context the context to use.
+     * @throws NamingException on subscription
+     */
+    public ServerTest(final InitialEventContext context) throws NamingException
+    {
+        super();
+        context.addNamingListener("/test", EventContext.OBJECT_SCOPE, this);
+    }
+
+    /**
+     * executes the ServerTest
+     * @param args the commandline arguments
+     */
+    public static void main(String[] args)
+    {
+        try
+        {
+            InitialEventContext context = new InitialEventContext();
+            context.bind("/test", "test");
+            new ServerTest(context);
+        }
+        catch (NamingException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    /** {@inheritDoc} */ @Override public  void objectChanged(NamingEvent evt)
+    {
+        System.out.println("changed " + evt);
+    }
+
+    /** {@inheritDoc} */ @Override public  void namingExceptionThrown(NamingExceptionEvent evt)
+    {
+        System.out.println("exception " + evt);
+    }
+
+    /** {@inheritDoc} */ @Override public  void objectAdded(NamingEvent evt)
+    {
+        System.out.println("added" + evt);
+    }
+
+    /** {@inheritDoc} */ @Override public  void objectRemoved(NamingEvent evt)
+    {
+        System.out.println("removed" + evt);
+    }
+
+    /** {@inheritDoc} */ @Override public  void objectRenamed(NamingEvent evt)
+    {
+        System.out.println("renamed : " + evt);
+    }
+}
