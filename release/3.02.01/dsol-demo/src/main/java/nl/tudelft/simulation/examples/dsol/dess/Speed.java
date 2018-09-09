0d5a72ca32b849fd3e2562ccd6bb69b4b467972e
averbraeck <averbraeck@f713b660-dc99-47c1-a074-784ff2779db6>
2018-09-09 17:06:44 +0000

    version 3.02.01

---
+++ release/3.02.01/dsol-demo/src/main/java/nl/tudelft/simulation/examples/dsol/dess/Speed.java
@@ -0,0 +1,40 @@
+package nl.tudelft.simulation.examples.dsol.dess;
+
+import java.rmi.RemoteException;
+
+import nl.tudelft.simulation.dsol.formalisms.dess.DifferentialEquation;
+import nl.tudelft.simulation.dsol.simtime.SimTimeDouble;
+import nl.tudelft.simulation.dsol.simulators.DESSSimulatorInterface;
+
+/**
+ * <p>
+ * Copyright (c) 2002-2018 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights
+ * reserved.
+ * <p>
+ * See for project information <a href="http://www.simulation.tudelft.nl/"> www.simulation.tudelft.nl</a>.
+ * <p>
+ * @version Aug 15, 2014 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ */
+public class Speed extends DifferentialEquation<Double, Double, SimTimeDouble>
+{
+    /** */
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * constructs a new Speed.
+     * @param simulator the simulator
+     * @throws RemoteException on network failureS
+     */
+    public Speed(final DESSSimulatorInterface.TimeDouble simulator) throws RemoteException
+    {
+        super(simulator);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public double[] dy(final double x, final double[] y)
+    {
+        return new double[]{0.5};
+    }
+}
