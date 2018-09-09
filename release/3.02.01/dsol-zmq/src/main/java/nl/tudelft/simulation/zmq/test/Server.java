7a68d42b1cef4820ed824229685031cecbaac4c7
averbraeck <averbraeck@f713b660-dc99-47c1-a074-784ff2779db6>
2018-09-09 17:33:48 +0000

    version 3.02.01

---
+++ release/3.02.01/dsol-zmq/src/main/java/nl/tudelft/simulation/zmq/test/Server.java
@@ -0,0 +1,49 @@
+package nl.tudelft.simulation.zmq.test;
+
+import org.zeromq.ZMQ;
+
+/**
+ * Server example for JeroMQ / ZeroMQ.
+ * <p>
+ * copyright (c) 2002-2018 <a href="http://www.simulation.tudelft.nl">Delft University of Technology</a>. <br>
+ * BSD-style license. See <a href="http://www.simulation.tudelft.nl/dsol/3.0/license.html">DSOL License</a>. <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @version Oct 21, 2016
+ */
+public class Server
+{
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        ZMQ.Context context = ZMQ.context(1);
+
+        // Socket to talk to clients
+        ZMQ.Socket responder = context.socket(ZMQ.REP);
+        responder.bind("tcp://*:5555");
+
+        while (!Thread.currentThread().isInterrupted())
+        {
+            // Wait for next request from the client
+            byte[] request = responder.recv(0);
+            System.out.println("Received " + request);
+
+            // Do some 'work'
+            try
+            {
+                Thread.sleep(1000);
+            }
+            catch (InterruptedException exception)
+            {
+                exception.printStackTrace();
+            }
+
+            // Send reply back to client
+            String reply = "World";
+            responder.send(reply.getBytes(), 0);
+        }
+        responder.close();
+        context.term();
+    }
+}
