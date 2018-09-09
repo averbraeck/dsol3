4b973eab91c2d7ff554fc0ecaada444537833ac5
averbraeck <averbraeck@f713b660-dc99-47c1-a074-784ff2779db6>
2018-09-09 17:34:16 +0000

    version 3.02.01

---
+++ release/3.02.01/dsol-event/src/test/java/nl/tudelft/simulation/event/EventProducerParent.java
@@ -0,0 +1,44 @@
+package nl.tudelft.simulation.event;
+
+/**
+ * The EventProducerParent is an event producer used in JUNIT tests.
+ * <p>
+ * (c) 2002-2018-2004 <a href="http://www.simulation.tudelft.nl">Delft University of Technology </a>, the
+ * Netherlands. <br>
+ * See for project information <a href="http://www.simulation.tudelft.nl">www.simulation.tudelft.nl </a> <br>
+ * License of use: <a href="http://www.gnu.org/copyleft/lesser.html">Lesser General Public License (LGPL) </a>, no
+ * warranty.
+ * @author <a href="https://www.linkedin.com/in/peterhmjacobs">Peter Jacobs </a>
+ * @version $Revision: 1.2 $ $Date: 2010/08/10 11:38:11 $
+ * @since 1.5
+ */
+public class EventProducerParent extends EventProducerChild
+{
+    /** The default serial version UID for serializable classes. */
+    private static final long serialVersionUID = 1L;
+
+    /** event_c is merely a test event. */
+    @SuppressWarnings("hiding")
+    public static final EventType EVENT_C = new EventType("EVENT_C");
+
+    /** event_d is merely a test event. */
+    protected static EventType eventD = new EventType("EVENT_D");
+
+    /** event_e is merely a test event. */
+    public static final EventType EVENT_E = new EventType("EVENT_E");
+
+    /**
+     * constructs a new EventProducerChild.
+     */
+    public EventProducerParent()
+    {
+        super();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public EventInterface fireEvent(final EventInterface event)
+    {
+        return super.fireEvent(event);
+    }
+}
---
+++ release/3.02.01/dsol-event/src/test/java/nl/tudelft/simulation/event/EventTest.java
@@ -0,0 +1,56 @@
+package nl.tudelft.simulation.event;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+/**
+ * The test script for the Event class.
+ * <p>
+ * (c) 2002-2018 <a href="http://www.simulation.tudelft.nl">Delft University of Technology </a>, the
+ * Netherlands. <br>
+ * See for project information <a href="http://www.simulation.tudelft.nl">www.simulation.tudelft.nl </a> <br>
+ * License of use: <a href="http://www.gnu.org/copyleft/lesser.html">Lesser General Public License (LGPL) </a>, no
+ * warranty.
+ * @author <a href="https://www.linkedin.com/in/peterhmjacobs">Peter Jacobs </a>
+ * @version $Revision: 1.2 $ $Date: 2010/08/10 11:38:11 $
+ * @since 1.5
+ */
+public class EventTest extends TestCase
+{
+    /** TEST_METHOD is the name of the test method. */
+    public static final String TEST_METHOD = "test";
+
+    /**
+     * constructs a new EventIteratorTest.
+     */
+    public EventTest()
+    {
+        this(TEST_METHOD);
+    }
+
+    /**
+     * constructs a new EventIteratorTest.
+     * @param method the name of the test method
+     */
+    public EventTest(final String method)
+    {
+        super(method);
+    }
+
+    /**
+     * tests the classes in the reference class.
+     */
+    public void test()
+    {
+        Object source = this;
+        EventType eventType = new EventType("TEST_TYPE");
+        Object content = new Object();
+
+        // Some basic tests
+        EventInterface event = new Event(eventType, source, content);
+        Assert.assertEquals(event.getContent(), content);
+        Assert.assertEquals(event.getSource(), source);
+        Assert.assertEquals(event.getType(), eventType);
+
+    }
+}
