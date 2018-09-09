1d6c9430da90f2b171deca1b1603b0bd803c641b
averbraeck <averbraeck@f713b660-dc99-47c1-a074-784ff2779db6>
2018-09-09 21:44:44 +0000

    version 3.02.01

---
+++ release/3.02.01/dsol-demo/src/main/java/nl/tudelft/simulation/dsol/tutorial/section44/BallAnimation2D.java
@@ -0,0 +1,72 @@
+package nl.tudelft.simulation.dsol.tutorial.section44;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.image.ImageObserver;
+import java.rmi.RemoteException;
+
+import javax.naming.NamingException;
+
+import nl.tudelft.simulation.dsol.animation.Locatable;
+import nl.tudelft.simulation.dsol.animation.D2.Renderable2D;
+import nl.tudelft.simulation.dsol.simulators.SimulatorInterface;
+
+/**
+ * The Animation of a Ball.
+ * <p>
+ * copyright (c) 2002-2018 <a href="https://simulation.tudelft.nl">Delft University of Technology </a>, the
+ * Netherlands. <br>
+ * See for project information <a href="https://simulation.tudelft.nl">www.simulation.tudelft.nl </a> <br>
+ * License of use: <a href="http://www.gnu.org/copyleft/lesser.html">Lesser General Public License (LGPL) </a>, no
+ * warranty.
+ * @author <a href="https://www.linkedin.com/in/peterhmjacobs">Peter Jacobs </a>
+ * @version 1.2 Apr 16, 2004
+ * @since 1.5
+ */
+public class BallAnimation2D extends Renderable2D<Locatable>
+{
+    /** the color of the ballAnimation. */
+    private Color color = Color.ORANGE;
+
+    /**
+     * constructs a new BallAnimation2D.
+     * @param source the source
+     * @param simulator the simulator
+     * @throws NamingException on animation error
+     * @throws RemoteException on animation error
+     */
+    public BallAnimation2D(final Locatable source, final SimulatorInterface.TimeDouble simulator)
+            throws RemoteException, NamingException
+    {
+        super(source, simulator);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void paint(final Graphics2D graphics, final ImageObserver observer)
+    {
+        graphics.setColor(this.color);
+        graphics.fillOval(-(int) Ball.RADIUS, -(int) Ball.RADIUS, (int) (Ball.RADIUS * 2.0), (int) (Ball.RADIUS * 2.0));
+        graphics.setFont(graphics.getFont().deriveFont(Font.BOLD));
+        graphics.setColor(Color.GRAY);
+        graphics.drawString(this.source.toString(), (int) (Ball.RADIUS * -1.0), (int) (Ball.RADIUS * 1.0));
+    }
+
+    /**
+     * @return Returns the color.
+     */
+    public Color getColor()
+    {
+        return this.color;
+    }
+
+    /**
+     * @param color The color to set.
+     */
+    public void setColor(final Color color)
+    {
+        this.color = color;
+    }
+
+}
