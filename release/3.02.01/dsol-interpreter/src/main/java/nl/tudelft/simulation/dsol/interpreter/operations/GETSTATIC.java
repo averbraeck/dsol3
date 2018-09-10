c45dcda1e08971e93c86712a03b823a09dc2fcc5
averbraeck <averbraeck@f713b660-dc99-47c1-a074-784ff2779db6>
2018-09-10 23:55:42 +0000

    version 3.02.01

---
+++ release/3.02.01/dsol-interpreter/src/main/java/nl/tudelft/simulation/dsol/interpreter/operations/GETSTATIC.java
@@ -0,0 +1,76 @@
+package nl.tudelft.simulation.dsol.interpreter.operations;
+
+import java.io.DataInput;
+import java.io.IOException;
+import java.lang.reflect.Field;
+
+import nl.tudelft.simulation.dsol.interpreter.InterpreterException;
+import nl.tudelft.simulation.dsol.interpreter.LocalVariable;
+import nl.tudelft.simulation.dsol.interpreter.OperandStack;
+import nl.tudelft.simulation.dsol.interpreter.classfile.Constant;
+import nl.tudelft.simulation.dsol.interpreter.classfile.ConstantFieldref;
+import nl.tudelft.simulation.language.reflection.ClassUtil;
+
+/**
+ * The GETSTATIC operation as defined in <a
+ * href="http://java.sun.com/docs/books/vmspec/2nd-edition/html/Instructions2.doc5.html">
+ * http://java.sun.com/docs/books/vmspec/2nd-edition/html/Instructions2.doc5.html </a>.
+ * <p>
+ * copyright (c) 2002-2018  <a href="https://simulation.tudelft.nl">Delft University of Technology</a>. <br>
+ * BSD-style license. See <a href="https://simulation.tudelft.nl/dsol/3.0/license.html">DSOL License</a>. <br>
+ * @author <a href="https://www.linkedin.com/in/peterhmjacobs">Peter Jacobs</a>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ */
+public class GETSTATIC extends VoidOperation
+{
+    /** OP refers to the operand code. */
+    public static final int OP = 178;
+
+    /** the index to load. */
+    private final int index;
+
+    /**
+     * constructs a new GETSTATIC.
+     * @param dataInput the dataInput
+     * @throws IOException on IOfailure
+     */
+    public GETSTATIC(final DataInput dataInput) throws IOException
+    {
+        super();
+        this.index = dataInput.readUnsignedShort();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final void execute(final OperandStack stack, final Constant[] constantPool,
+            final LocalVariable[] localVariables)
+    {
+        try
+        {
+            Field field = null;
+            ConstantFieldref constantFieldref = (ConstantFieldref) constantPool[this.index];
+            Class<?> referenceClass = constantFieldref.getConstantClass().getValue().getClassValue();
+            field = ClassUtil.resolveField(referenceClass, constantFieldref.getConstantNameAndType().getName());
+            field.setAccessible(true);
+            stack.push(field.get(null));
+        }
+        catch (Exception exception)
+        {
+            throw new InterpreterException(exception);
+        }
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final int getByteLength()
+    {
+        return OPCODE_BYTE_LENGTH + 2;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final int getOpcode()
+    {
+        return GETSTATIC.OP;
+    }
+}
