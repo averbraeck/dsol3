80b6772672f6f9b93b9f36861e38bfbc5e98b617
averbraeck <averbraeck@f713b660-dc99-47c1-a074-784ff2779db6>
2018-09-10 23:55:22 +0000

    version 3.02.01

---
+++ release/3.02.01/dsol-base/src/test/java/nl/tudelft/simulation/language/BoundingBoxTest.java
@@ -0,0 +1,46 @@
+package nl.tudelft.simulation.language;
+
+import junit.framework.TestCase;
+
+/**
+ * The JUNIT Test for the <code>BoundingBox</code>.
+ * <p>
+ * (c) 2002-2018 <a href="https://simulation.tudelft.nl">Delft University of Technology </a>, the
+ * Netherlands.
+ * <p>
+ * See for project information <a
+ * href="https://simulation.tudelft.nl/dsol/language">www.simulation.tudelft.nl/language </a> <br>
+ * License of use: <a href="http://www.gnu.org/copyleft/lesser.html">Lesser General Public License (LGPL) </a>, no
+ * warranty
+ * @author <a href="https://www.linkedin.com/in/peterhmjacobs">Peter Jacobs </a>
+ * @version 1.2 Sep 28, 2004
+ * @since 1.5
+ */
+public class BoundingBoxTest extends TestCase
+{
+
+    /**
+     * constructs a new MonitorTest.
+     */
+    public BoundingBoxTest()
+    {
+        this("test");
+    }
+
+    /**
+     * constructs a new MonitorTest.
+     * @param arg0
+     */
+    public BoundingBoxTest(String arg0)
+    {
+        super(arg0);
+    }
+
+    /**
+     * tests the BoundingBox
+     */
+    public void test()
+    {
+        // TODO implement the test method
+    }
+}
