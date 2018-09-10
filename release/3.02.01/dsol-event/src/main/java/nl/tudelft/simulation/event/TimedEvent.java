5ac8fdbcf88e02ce1f28f166288f2e8f8a58b867
averbraeck <averbraeck@f713b660-dc99-47c1-a074-784ff2779db6>
2018-09-10 01:26:34 +0000

    version 3.02.01

---
+++ release/3.02.01/dsol-event/src/main/java/nl/tudelft/simulation/event/TimedEvent.java
@@ -0,0 +1,56 @@
+package nl.tudelft.simulation.event;
+
+/**
+ * The TimedEvent is the reference implementation for a timed event.
+ * <p>
+ * copyright (c) 2002-2018  <a href="https://simulation.tudelft.nl">Delft University of Technology</a>. <br>
+ * BSD-style license. See <a href="https://simulation.tudelft.nl/dsol/3.0/license.html">DSOL License</a>. <br>
+ * @author <a href="https://www.linkedin.com/in/peterhmjacobs">Peter Jacobs</a>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @param <T> the object type of the timestamp
+ * @since 1.5
+ */
+public class TimedEvent<T extends Comparable<T>> extends Event implements Comparable<TimedEvent<T>>
+{
+    /** The default serial version UID for serializable classes. */
+    private static final long serialVersionUID = 20140826L;
+
+    /** timeStamp refers to the time stamp of the event. */
+    private final T timeStamp;
+
+    /**
+     * constructs a new timed event.
+     * @param type the eventType of the event.
+     * @param source the source of the event.
+     * @param value the value of the event.
+     * @param timeStamp the timeStamp.
+     */
+    public TimedEvent(final EventType type, final Object source, final Object value, final T timeStamp)
+    {
+        super(type, source, value);
+        this.timeStamp = timeStamp;
+    }
+
+    /**
+     * returns the timeStamp of this event.
+     * @return the timestamp as double.
+     */
+    public T getTimeStamp()
+    {
+        return this.timeStamp;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public int compareTo(TimedEvent<T> o)
+    {
+        return this.timeStamp.compareTo(o.getTimeStamp());
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String toString()
+    {
+        return super.toString().split("]")[0] + ";" + this.getTimeStamp() + "]";
+    }
+}
