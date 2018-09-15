c3183172917990e096518628c54e7f8bbc2e00cc
averbraeck <averbraeck@f713b660-dc99-47c1-a074-784ff2779db6>
2018-09-15 15:32:16 +0000

    version 3.02.02

---
+++ release/3.02.02/dsol-interpreter/src/main/java/nl/tudelft/simulation/dsol/interpreter/operations/F2I.java
@@ -0,0 +1,52 @@
+package nl.tudelft.simulation.dsol.interpreter.operations;
+
+import nl.tudelft.simulation.dsol.interpreter.LocalVariable;
+import nl.tudelft.simulation.dsol.interpreter.OperandStack;
+import nl.tudelft.simulation.dsol.interpreter.classfile.Constant;
+
+/**
+ * The F2I operation as defined in <a
+ * href="http://java.sun.com/docs/books/vmspec/2nd-edition/html/Instructions2.doc4.html">
+ * http://java.sun.com/docs/books/vmspec/2nd-edition/html/Instructions2.doc4.html </a>.
+ * <p>
+ * copyright (c) 2002-2018  <a href="https://simulation.tudelft.nl">Delft University of Technology</a>. <br>
+ * BSD-style license. See <a href="https://simulation.tudelft.nl/dsol/3.0/license.html">DSOL License</a>. <br>
+ * @author <a href="https://www.linkedin.com/in/peterhmjacobs">Peter Jacobs</a>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ */
+public class F2I extends VoidOperation
+{
+    /** OP refers to the operand code. */
+    public static final int OP = 139;
+
+    /**
+     * constructs a new F2I.
+     */
+    public F2I()
+    {
+        super();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final void execute(final OperandStack stack, final Constant[] constantPool,
+            final LocalVariable[] localVariables)
+    {
+        float f = ((Number) stack.pop()).floatValue();
+        stack.push(Integer.valueOf((int) f));
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final int getByteLength()
+    {
+        return OPCODE_BYTE_LENGTH;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final int getOpcode()
+    {
+        return F2I.OP;
+    }
+}
