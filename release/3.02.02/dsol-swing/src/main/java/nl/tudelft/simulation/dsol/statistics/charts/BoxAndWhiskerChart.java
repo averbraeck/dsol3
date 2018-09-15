b54530adc7a25864783ab49d8d47ad532bbafb49
averbraeck <averbraeck@f713b660-dc99-47c1-a074-784ff2779db6>
2018-09-15 09:49:14 +0000

    version 3.02.02

---
+++ release/3.02.02/dsol-swing/src/main/java/nl/tudelft/simulation/dsol/statistics/charts/BoxAndWhiskerChart.java
@@ -0,0 +1,47 @@
+package nl.tudelft.simulation.dsol.statistics.charts;
+
+import javax.naming.Context;
+import javax.naming.NamingException;
+
+import org.pmw.tinylog.Logger;
+
+import nl.tudelft.simulation.dsol.simulators.SimulatorInterface;
+import nl.tudelft.simulation.naming.context.ContextUtil;
+
+/**
+ * The BoxAndWhiskerPlot extends the BoxAndWhiskerPlot and links this it to the dsol framework <br>
+ * (c) 2002-2018 <a href="https://simulation.tudelft.nl">Delft University of Technology </a>, the Netherlands. <br>
+ * See for project information <a href="https://simulation.tudelft.nl">www.simulation.tudelft.nl </a> <br>
+ * License of use: <a href="http://www.gnu.org/copyleft/lesser.html">Lesser General Public License (LGPL) </a>, no
+ * warranty.
+ * @version $Revision: 1.1 $ $Date: 2010/08/10 11:37:21 $
+ * @author <a href="https://www.linkedin.com/in/peterhmjacobs">Peter Jacobs </a>
+ */
+public class BoxAndWhiskerChart extends nl.tudelft.simulation.jstats.charts.boxAndWhisker.BoxAndWhiskerChart
+{
+    /**
+     * constructs a new BoxAndWhiskerChart.
+     * @param simulator the simulator
+     * @param title the title
+     */
+    public BoxAndWhiskerChart(final SimulatorInterface simulator, final String title)
+    {
+        super(title);
+        try
+        {
+            Context context = ContextUtil.lookup(simulator.getReplication().getContext(), "/charts");
+            ContextUtil.bind(context, this);
+        }
+        catch (NamingException exception)
+        {
+            Logger.warn(exception, "<init>");
+        }
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String toString()
+    {
+        return super.getChart().getTitle().getText();
+    }
+}
