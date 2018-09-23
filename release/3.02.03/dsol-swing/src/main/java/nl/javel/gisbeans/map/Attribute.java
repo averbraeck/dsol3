5e6fce9a171ec5a8a22e873dfde1719c89947c85
averbraeck <averbraeck@f713b660-dc99-47c1-a074-784ff2779db6>
2018-09-23 01:17:09 +0000

    Deploy version 3.02.03 with CategoryLogger and SimLogger

---
+++ release/3.02.03/dsol-swing/src/main/java/nl/javel/gisbeans/map/Attribute.java
@@ -0,0 +1,87 @@
+package nl.javel.gisbeans.map;
+
+import nl.tudelft.simulation.dsol.logger.SimLogger;
+
+/**
+ * An attribute.
+ * <p>
+ * (c) 2002-2018 <a href="https://simulation.tudelft.nl">Delft University of Technology </a>, the
+ * Netherlands. <br>
+ * See for project information <a href="https://simulation.tudelft.nl">www.simulation.tudelft.nl </a> <br>
+ * License of use: <a href="http://www.gnu.org/copyleft/lesser.html">Lesser General Public License (LGPL) </a>, no
+ * warranty.
+ * @author <a href="http://www.tbm.tudelft.nl/webstaf/peterja/index.htm">Peter Jacobs </a>
+ * @version 1.2 Jun 22, 2004
+ * @since 1.4
+ */
+public class Attribute extends AbstractAttribute
+{
+    /** RADIANS */
+    public static final short RADIANS = 0;
+
+    /** DEGREES */
+    public static final short DEGREES = 1;
+
+    /** the angleColumn. */
+    private int angleColumn = 0;
+
+    /** the valueColumn. */
+    private int valueColumn = 0;
+
+    /** the mode of the angle. */
+    private short mode = RADIANS;
+
+    /**
+     * constructs a new Attribute.
+     * @param layer the layer to add the attribute to
+     * @param mode the mode (degrees or radians)
+     * @param angleColumn the angleColumn
+     * @param valueColumn the valueColumn
+     */
+    public Attribute(LayerInterface layer, short mode, int angleColumn, int valueColumn)
+    {
+        super(layer);
+        this.angleColumn = angleColumn;
+        this.valueColumn = valueColumn;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public double getAngle(int shapeIndex)
+    {
+        if (this.angleColumn == -1)
+        {
+            return 0.0;
+        }
+        try
+        {
+            double angle =
+                    Double.parseDouble(super.layer.getDataSource().getAttributes()[shapeIndex][this.angleColumn]);
+            if (this.mode == DEGREES)
+            {
+                angle = Math.toRadians(angle);
+            }
+            return angle;
+        }
+        catch (Exception exception)
+        {
+            SimLogger.always().error(exception);
+            return 0.0;
+        }
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String getValue(int shapeIndex)
+    {
+        try
+        {
+            return super.layer.getDataSource().getAttributes()[shapeIndex][this.valueColumn];
+        }
+        catch (Exception exception)
+        {
+            SimLogger.always().error(exception);
+            return "";
+        }
+    }
+}
