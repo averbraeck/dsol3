60f6b58b54b426cc47456a13ebce343af26582af
averbraeck <averbraeck@f713b660-dc99-47c1-a074-784ff2779db6>
2018-09-30 01:18:11 +0000

    deployed dsol version 3.02.04

---
+++ release/3.02.04/dsol-zmq/src/main/java/nl/tudelft/simulation/zmq/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Generic classses for linking DSOL to ZeroMQ.
+ * <p>
+ * copyright (c) 2002-2018 <a href="https://simulation.tudelft.nl">Delft University of Technology</a>. <br>
+ * BSD-style license. See <a href="https://simulation.tudelft.nl/dsol/3.0/license.html">DSOL License</a>. <br>
+ * @author <a href="https://www.linkedin.com/in/peterhmjacobs">Peter Jacobs</a>
+ * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @version Oct 21, 2016
+ */
+package nl.tudelft.simulation.zmq;
