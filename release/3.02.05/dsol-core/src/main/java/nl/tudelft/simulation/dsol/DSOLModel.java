ae008496be02f9d113c0145912582cb3fada209e
averbraeck <averbraeck@f713b660-dc99-47c1-a074-784ff2779db6>
2018-09-30 17:25:19 +0000

    deployed dsol version 3.02.05

---
+++ release/3.02.05/dsol-core/src/main/java/nl/tudelft/simulation/dsol/DSOLModel.java
@@ -0,0 +1,106 @@
+package nl.tudelft.simulation.dsol;
+
+import java.io.Serializable;
+import java.util.Calendar;
+
+import org.djunits.value.vdouble.scalar.Duration;
+import org.djunits.value.vdouble.scalar.Time;
+import org.djunits.value.vfloat.scalar.FloatDuration;
+import org.djunits.value.vfloat.scalar.FloatTime;
+
+import nl.tudelft.simulation.dsol.simtime.SimTime;
+import nl.tudelft.simulation.dsol.simtime.SimTimeCalendarDouble;
+import nl.tudelft.simulation.dsol.simtime.SimTimeCalendarFloat;
+import nl.tudelft.simulation.dsol.simtime.SimTimeCalendarLong;
+import nl.tudelft.simulation.dsol.simtime.SimTimeDouble;
+import nl.tudelft.simulation.dsol.simtime.SimTimeDoubleUnit;
+import nl.tudelft.simulation.dsol.simtime.SimTimeFloat;
+import nl.tudelft.simulation.dsol.simtime.SimTimeFloatUnit;
+import nl.tudelft.simulation.dsol.simtime.SimTimeLong;
+import nl.tudelft.simulation.dsol.simulators.SimulatorInterface;
+
+/**
+ * The model interface defines the model object. Since version 2.1.0 of DSOL, the ModelInterface now knows its simulator
+ * and can return it to anyone interested. Through the Simulator, the Replication can be requested and through that the
+ * Experiment and the Treatment under which the simulation is running.
+ * <p>
+ * Copyright (c) 2003-2018 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights
+ * reserved. See for project information <a href="https://simulation.tudelft.nl/" target="_blank">
+ * https://simulation.tudelft.nl</a>. The DSOL project is distributed under a three-clause BSD-style license, which can
+ * be found at <a href="https://simulation.tudelft.nl/dsol/3.0/license.html" target="_blank">
+ * https://simulation.tudelft.nl/dsol/3.0/license.html</a>.
+ * </p>
+ * @author <a href="https://www.linkedin.com/in/peterhmjacobs">Peter Jacobs </a>
+ * @param <A> the absolute storage type for the simulation time, e.g. Calendar, Duration, or Double.
+ * @param <R> the relative type for time storage, e.g. Long for the Calendar. For most non-calendar types, the absolute
+ *            and relative types are the same.
+ * @param <T> the extended type itself to be able to implement a comparator on the simulation time.
+ * @since 1.5
+ */
+public interface DSOLModel<A extends Comparable<A>, R extends Number & Comparable<R>, T extends SimTime<A, R, T>>
+        extends Serializable
+{
+    /**
+     * construct a model on a simulator.
+     * @param simulator is the simulator
+     * @throws SimRuntimeException on model failure
+     */
+    void constructModel(SimulatorInterface<A, R, T> simulator) throws SimRuntimeException;
+
+    /**
+     * @return the simulator for the model
+     */
+    SimulatorInterface<A, R, T> getSimulator();
+
+    /***********************************************************************************************************/
+    /*********************************** EASY ACCESS INTERFACE EXTENSIONS **************************************/
+    /***********************************************************************************************************/
+
+    /** Easy access interface ModelInterface.TimeDouble. */
+    public interface TimeDouble extends DSOLModel<Double, Double, SimTimeDouble>
+    {
+        // typed extension
+    }
+
+    /** Easy access interface ModelInterface.TimeFloat. */
+    public interface TimeFloat extends DSOLModel<Float, Float, SimTimeFloat>
+    {
+        // typed extension
+    }
+
+    /** Easy access interface ModelInterface.TimeLong. */
+    public interface TimeLong extends DSOLModel<Long, Long, SimTimeLong>
+    {
+        // typed extension
+    }
+
+    /** Easy access interface ModelInterface.TimeDoubleUnit. */
+    public interface TimeDoubleUnit extends DSOLModel<Time, Duration, SimTimeDoubleUnit>
+    {
+        // typed extension
+    }
+
+    /** Easy access interface ModelInterface.TimeFloatUnit. */
+    public interface TimeFloatUnit extends DSOLModel<FloatTime, FloatDuration, SimTimeFloatUnit>
+    {
+        // typed extension
+    }
+
+    /** Easy access interface ModelInterface.TimeCalendarDouble. */
+    public interface CalendarDouble extends DSOLModel<Calendar, Duration, SimTimeCalendarDouble>
+    {
+        // typed extension
+    }
+
+    /** Easy access interface ModelInterface.TimeCalendarFloat. */
+    public interface CalendarFloat extends DSOLModel<Calendar, FloatDuration, SimTimeCalendarFloat>
+    {
+        // typed extension
+    }
+
+    /** Easy access interface ModelInterface.TimeCalendarLong. */
+    public interface CalendarLong extends DSOLModel<Calendar, Long, SimTimeCalendarLong>
+    {
+        // typed extension
+    }
+}
---
+++ release/3.02.05/dsol-core/src/test/java/nl/tudelft/simulation/dsol/simulators/TestModel.java
@@ -0,0 +1,80 @@
+package nl.tudelft.simulation.dsol.simulators;
+
+import nl.tudelft.simulation.dsol.DSOLModel;
+import nl.tudelft.simulation.event.EventInterface;
+import nl.tudelft.simulation.event.EventListenerInterface;
+
+/**
+ * The TestModel.
+ * <p>
+ * Copyright (c) 2002-2018 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights
+ * reserved. See for project information <a href="https://simulation.tudelft.nl/" target="_blank">
+ * https://simulation.tudelft.nl</a>. The DSOL project is distributed under a three-clause BSD-style license, which can
+ * be found at <a href="https://simulation.tudelft.nl/dsol/3.0/license.html" target="_blank">
+ * https://simulation.tudelft.nl/dsol/3.0/license.html</a>.
+ * </p>
+ * @author <a href="https://www.linkedin.com/in/peterhmjacobs">Peter Jacobs </a>,
+ *         <a href="mailto:a.verbraeck@tudelft.nl">Alexander Verbraeck </a>
+ */
+public class TestModel implements DSOLModel, EventListenerInterface
+{
+
+    /** the startTime. */
+    private long startTime = 0L;
+
+    /** the simulator. */
+    private SimulatorInterface simulator;
+
+    /**
+     * constructs a new TestModel.
+     */
+    public TestModel()
+    {
+        super();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void constructModel(final SimulatorInterface simulator)
+    {
+        this.simulator = simulator;
+        try
+        {
+            simulator.addListener(this, SimulatorInterface.END_REPLICATION_EVENT, false);
+            simulator.addListener(this, SimulatorInterface.START_REPLICATION_EVENT, false);
+            simulator.addListener(this, SimulatorInterface.START_EVENT, false);
+            simulator.addListener(this, SimulatorInterface.STOP_EVENT, false);
+            simulator.addListener(this, SimulatorInterface.STEP_EVENT, false);
+            simulator.addListener(this, SimulatorInterface.TIME_CHANGED_EVENT, false);
+        }
+        catch (Exception exception)
+        {
+            exception.printStackTrace();
+        }
+    }
+
+    /**
+     * @return the simulator
+     */
+    public SimulatorInterface getSimulator()
+    {
+        return this.simulator;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void notify(final EventInterface event)
+    {
+        if (event.getType().equals(SimulatorInterface.START_EVENT))
+        {
+            this.startTime = System.currentTimeMillis();
+            System.out.println("started @ " + this.startTime);
+        }
+        if (event.getType().equals(SimulatorInterface.STOP_EVENT))
+        {
+            long runLength = System.currentTimeMillis() - this.startTime;
+            System.out.println(
+                    "runlength=" + runLength + " time=" + ((SimulatorInterface) event.getSource()).getSimulatorTime());
+        }
+    }
+}
