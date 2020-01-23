8165a5cd22a3b1549d0edb696861bc9e4b6e2382
averbraeck <averbraeck@f713b660-dc99-47c1-a074-784ff2779db6>
2020-01-23 09:39:26 +0000

    Small change in the Lotka-Volterra demo.

--- trunk/dsol-demo/src/main/java/nl/tudelft/simulation/dsol/tutorial/section43/PredatorPreyModel.java
+++ trunk/dsol-demo/src/main/java/nl/tudelft/simulation/dsol/tutorial/section43/PredatorPreyModel.java
@@ -1,17 +1,20 @@
 package nl.tudelft.simulation.dsol.tutorial.section43;
 
 import java.io.Serializable;
+import java.rmi.RemoteException;
+
+import org.djutils.event.EventInterface;
+import org.djutils.event.EventListenerInterface;
+import org.djutils.event.EventProducer;
 
 import nl.tudelft.simulation.dsol.SimRuntimeException;
 import nl.tudelft.simulation.dsol.formalisms.dess.DifferentialEquationInterface;
 import nl.tudelft.simulation.dsol.model.AbstractDSOLModel;
-import nl.tudelft.simulation.dsol.simtime.SimTimeDouble;
 import nl.tudelft.simulation.dsol.simulators.DESSSimulator;
-import nl.tudelft.simulation.dsol.statistics.SimPersistent;
 import nl.tudelft.simulation.dsol.swing.charts.xy.XYChart;
 
 /**
- * A Life.
+ * A Predator-Prey model with a graph.
  * <p>
  * Copyright (c) 2002-2020 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
  * for project information <a href="https://simulation.tudelft.nl/" target="_blank"> https://simulation.tudelft.nl</a>. The DSOL
@@ -47,17 +50,30 @@ public class PredatorPreyModel extends AbstractDSOLModel.TimeDouble<DESSSimulato
             // Prey and Predator definitions
             Population population = new Population(this.simulator);
 
-            SimPersistent<Double, Double, SimTimeDouble> preyPopulation = new SimPersistent<>("prey population", this.simulator,
-                    population, DifferentialEquationInterface.VALUE_CHANGED_EVENT[0]);
-            preyPopulation.initialize();
+            // This would work:
+            // SimPersistent<Double, Double, SimTimeDouble> preyPopulation = new SimPersistent<>("prey population",
+            // this.simulator, population, DifferentialEquationInterface.VALUE_CHANGED_EVENT[0]);
+            // preyPopulation.initialize();
+            // SimPersistent<Double, Double, SimTimeDouble> predatorPopulation = new SimPersistent<>("predator population",
+            // this.simulator, population, DifferentialEquationInterface.VALUE_CHANGED_EVENT[1]);
+            // predatorPopulation.initialize();
+            // this.chart = new XYChart(this.simulator, "population");
+            // this.chart.add(preyPopulation);
+            // this.chart.add(predatorPopulation);
 
-            SimPersistent<Double, Double, SimTimeDouble> predatorPopulation = new SimPersistent<>("predator population",
-                    this.simulator, population, DifferentialEquationInterface.VALUE_CHANGED_EVENT[1]);
-            predatorPopulation.initialize();
+            // This would work as well:
+            // this.chart = new XYChart(this.simulator, "population");
+            // this.chart.add("prey population", population, DifferentialEquationInterface.VALUE_CHANGED_EVENT[0]);
+            // this.chart.add("predator population", population, DifferentialEquationInterface.VALUE_CHANGED_EVENT[1]);
 
+            // this works 8 times faster because the graph gets fewer data points:
             this.chart = new XYChart(this.simulator, "population");
-            this.chart.add(preyPopulation);
-            this.chart.add(predatorPopulation);
+            SkipEventProducer preySEP = new SkipEventProducer(8);
+            population.addListener(preySEP, DifferentialEquationInterface.VALUE_CHANGED_EVENT[0]);
+            this.chart.add("prey population", preySEP, DifferentialEquationInterface.VALUE_CHANGED_EVENT[0]);
+            SkipEventProducer predSEP = new SkipEventProducer(8);
+            population.addListener(predSEP, DifferentialEquationInterface.VALUE_CHANGED_EVENT[1]);
+            this.chart.add("predator population", predSEP, DifferentialEquationInterface.VALUE_CHANGED_EVENT[1]);
         }
         catch (Exception exception)
         {
@@ -78,6 +94,47 @@ public class PredatorPreyModel extends AbstractDSOLModel.TimeDouble<DESSSimulato
     public Serializable getSourceId()
     {
         return "PredatorPreyModel";
+    }
+
+    /** small event producer that fires events for the graph with a lower resolution. */
+    static class SkipEventProducer extends EventProducer implements EventListenerInterface
+    {
+        /** */
+        private static final long serialVersionUID = 1L;
+
+        /** */
+        private int count = 0;
+
+        /** */
+        private final int skip;
+
+        /**
+         * @param skip the "mod" factor used. When skip = 10, one in 10 events will be re-fired.
+         */
+        SkipEventProducer(final int skip)
+        {
+            super();
+            this.skip = skip;
+        }
+
+        /** {@inheritDoc} */
+        @Override
+        public void notify(final EventInterface event) throws RemoteException
+        {
+            if (this.count % this.skip == 0)
+            {
+                fireEvent(event);
+                this.count = 0;
+            }
+            this.count++;
+        }
+
+        /** {@inheritDoc} */
+        @Override
+        public Serializable getSourceId()
+        {
+            return "SkipEventProducer";
+        }
     }
 
 }
