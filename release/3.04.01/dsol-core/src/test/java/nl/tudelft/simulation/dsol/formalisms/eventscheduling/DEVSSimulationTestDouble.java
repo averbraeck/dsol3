8c404e7e164bb9c1ab63e41356b5ef550deba5d1
averbraeck <averbraeck@f713b660-dc99-47c1-a074-784ff2779db6>
2020-01-24 03:16:05 +0000

    deployed DSOL version 3.04.01

---
+++ release/3.04.01/dsol-core/src/test/java/nl/tudelft/simulation/dsol/formalisms/eventscheduling/DEVSSimulationTestDouble.java
@@ -0,0 +1,208 @@
+package nl.tudelft.simulation.dsol.formalisms.eventscheduling;
+
+import java.io.Serializable;
+import java.rmi.RemoteException;
+import java.util.concurrent.TimeoutException;
+
+import javax.naming.NamingException;
+
+import org.djutils.event.EventInterface;
+import org.djutils.event.EventListenerInterface;
+import org.junit.Test;
+
+import net.jodah.concurrentunit.Waiter;
+import nl.tudelft.simulation.dsol.SimRuntimeException;
+import nl.tudelft.simulation.dsol.experiment.Replication;
+import nl.tudelft.simulation.dsol.experiment.ReplicationMode;
+import nl.tudelft.simulation.dsol.model.AbstractDSOLModel;
+import nl.tudelft.simulation.dsol.simulators.DEVSSimulator;
+import nl.tudelft.simulation.dsol.simulators.DEVSSimulatorInterface;
+import nl.tudelft.simulation.dsol.simulators.SimulatorInterface;
+import nl.tudelft.simulation.jstats.distributions.DistContinuous;
+import nl.tudelft.simulation.jstats.distributions.DistUniform;
+import nl.tudelft.simulation.jstats.streams.MersenneTwister;
+import nl.tudelft.simulation.jstats.streams.StreamInterface;
+
+/**
+ * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ */
+public class DEVSSimulationTestDouble implements EventListenerInterface
+{
+    /** */
+    protected DEVSSimulatorInterface.TimeDouble devsSimulator;
+
+    /** the Waiter from ConcurrentUnit that catches AssertionErrors in other threads. */
+    protected Waiter waiter;
+
+    /**
+     * @throws SimRuntimeException in case of error
+     * @throws RemoteException in case of error
+     * @throws NamingException in case of error
+     * @throws InterruptedException on error
+     * @throws TimeoutException on error
+     */
+    @Test
+    public void testDEVSSimulationDouble()
+            throws SimRuntimeException, RemoteException, NamingException, TimeoutException, InterruptedException
+    {
+        this.waiter = new Waiter();
+        this.devsSimulator = new DEVSSimulator.TimeDouble("testDEVSSimulationDouble");
+        this.devsSimulator.addListener(this, SimulatorInterface.END_REPLICATION_EVENT);
+        ModelDouble model = new ModelDouble(this.devsSimulator);
+        Replication.TimeDouble<DEVSSimulatorInterface.TimeDouble> rep =
+                Replication.TimeDouble.create("rep1", 0.0, 0.0, 100.0, model);
+        this.devsSimulator.initialize(rep, ReplicationMode.TERMINATING);
+        this.devsSimulator.scheduleEventAbs(1.0, this, this, "step1", new Object[] {1.0});
+        this.devsSimulator.start();
+        this.waiter.await(1000);
+    }
+
+    /**
+     * Do a simulation step and check the time.
+     * @param checkTime the step on the simulator
+     * @throws SimRuntimeException on error
+     */
+    protected void step1(final double checkTime) throws SimRuntimeException
+    {
+        this.waiter.assertEquals(this.devsSimulator.getSimulatorTime(), checkTime, 0.0001);
+        this.devsSimulator.scheduleEventRel(1.0, this, this, "step1", new Object[] {checkTime + 1.0});
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void notify(EventInterface event) throws RemoteException
+    {
+        this.waiter.resume();
+    }
+
+    /**
+     * @throws SimRuntimeException in case of error
+     * @throws RemoteException in case of error
+     * @throws NamingException in case of error
+     * @throws InterruptedException on error
+     * @throws TimeoutException on error
+     */
+    @Test
+    public void testRunUpTo()
+            throws SimRuntimeException, RemoteException, NamingException, TimeoutException, InterruptedException
+    {
+        this.waiter = new Waiter();
+        this.devsSimulator = new DEVSSimulator.TimeDouble("testRunUpTo");
+        this.devsSimulator.addListener(this, SimulatorInterface.END_REPLICATION_EVENT);
+        ModelDouble model = new ModelDouble(this.devsSimulator);
+        Replication.TimeDouble<DEVSSimulatorInterface.TimeDouble> rep =
+                Replication.TimeDouble.create("rep1", 0.0, 0.0, 1000.0, model);
+        this.devsSimulator.initialize(rep, ReplicationMode.TERMINATING);
+        final DEVSSimulatorInterface.TimeDouble sim = this.devsSimulator;
+        final Waiter w = this.waiter;
+        final Object target = this;
+
+        new Thread(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                StreamInterface stream = new MersenneTwister();
+                DistContinuous dist = new DistUniform(stream, 0, 1000);
+                for (int i = 0; i < 1000; i++)
+                {
+                    double time = dist.draw();
+                    sim.scheduleEventAbs(time, this, target, "doWork", new Object[] {time});
+                }
+                for (double t = 0.0; t < 1000.0; t += 1.0)
+                {
+                    sim.runUpTo(t);
+                    while (sim.isRunning())
+                    {
+                        try
+                        {
+                            Thread.sleep(0, 10);
+                        }
+                        catch (InterruptedException exception)
+                        {
+                            w.fail(exception);
+                        }
+                    }
+                    w.assertEquals(t, sim.getSimulatorTime(), 0.0001);
+                }
+                sim.start();
+            }
+        }).start();
+        this.waiter.await(10000);
+    }
+
+    /**
+     * do some work.
+     * @param time the expected time when the event should be executed
+     */
+    protected void doWork(final double time)
+    {
+        this.waiter.assertEquals(time, this.devsSimulator.getSimulatorTime(), 0.0001);
+    }
+
+    /**
+     * @throws SimRuntimeException in case of error
+     * @throws RemoteException in case of error
+     * @throws NamingException in case of error
+     * @throws InterruptedException on error
+     * @throws TimeoutException on error
+     */
+    @Test
+    public void testSimLambda()
+            throws SimRuntimeException, RemoteException, NamingException, TimeoutException, InterruptedException
+    {
+        this.waiter = new Waiter();
+        this.devsSimulator = new DEVSSimulator.TimeDouble("testSimLambda");
+        this.devsSimulator.addListener(this, SimulatorInterface.END_REPLICATION_EVENT);
+        ModelDouble model = new ModelDouble(this.devsSimulator);
+        Replication.TimeDouble<DEVSSimulatorInterface.TimeDouble> rep =
+                Replication.TimeDouble.create("rep1", 0.0, 0.0, 100.0, model);
+        this.devsSimulator.initialize(rep, ReplicationMode.TERMINATING);
+
+        for (int i = 0; i < 10; i++)
+        {
+            this.devsSimulator.scheduleEventAbs(1.0d * i, new Executable()
+            {
+                @Override
+                public void execute()
+                {
+                    DEVSSimulationTestDouble.this.waiter
+                            .assertTrue(DEVSSimulationTestDouble.this.devsSimulator.getSimulatorTime() <= 10.0);
+                }
+            });
+        }
+        this.devsSimulator.start();
+        this.waiter.await(1000);
+    }
+
+    /**
+     * THE MODEL.
+     */
+    public static class ModelDouble extends AbstractDSOLModel.TimeDouble<DEVSSimulatorInterface.TimeDouble>
+    {
+        /** */
+        private static final long serialVersionUID = 1L;
+
+        /**
+         * @param simulator the simulator.
+         */
+        public ModelDouble(final DEVSSimulatorInterface.TimeDouble simulator)
+        {
+            super(simulator);
+        }
+
+        /** {@inheritDoc} */
+        @Override
+        public void constructModel() throws SimRuntimeException
+        {
+            //
+        }
+
+        /** {@inheritDoc} */
+        @Override
+        public Serializable getSourceId()
+        {
+            return "ModelDouble";
+        }
+    }
+}
---
+++ release/3.04.01/dsol-core/src/test/java/nl/tudelft/simulation/jstats/statistics/PersistentTest.java
@@ -0,0 +1,114 @@
+package nl.tudelft.simulation.jstats.statistics;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.djutils.event.Event;
+import org.djutils.event.TimedEvent;
+import org.junit.Test;
+
+/**
+ * The PersistentTest test the persistent
+ * <p>
+ * Copyright (c) 2002-2020 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
+ * for project information <a href="https://simulation.tudelft.nl/" target="_blank"> https://simulation.tudelft.nl</a>. The DSOL
+ * project is distributed under a three-clause BSD-style license, which can be found at
+ * <a href="https://simulation.tudelft.nl/dsol/3.0/license.html" target="_blank">
+ * https://simulation.tudelft.nl/dsol/3.0/license.html</a>.
+ * </p>
+ * @author <a href="https://www.linkedin.com/in/peterhmjacobs">Peter Jacobs </a>
+ * @since 1.5
+ */
+public class PersistentTest
+{
+    /**
+     * tests the persistent.
+     */
+    @Test
+    public void testPersistent()
+    {
+        String description = "THIS PERSISTENT IS TESTED";
+        Persistent persistent = new Persistent(description);
+
+        // check the description
+        assertTrue(persistent.toString().equals(description));
+
+        // now we check the initial values
+        assertTrue(Double.valueOf(persistent.getMin()).isNaN());
+        assertTrue(Double.valueOf(persistent.getMax()).isNaN());
+        assertTrue(Double.valueOf(persistent.getSampleMean()).isNaN());
+        assertTrue(Double.valueOf(persistent.getSampleVariance()).isNaN());
+        assertTrue(Double.valueOf(persistent.getStdDev()).isNaN());
+        assertTrue(Double.valueOf(persistent.getSum()).isNaN());
+        assertTrue(persistent.getN() == Long.MIN_VALUE);
+        assertTrue(persistent.getConfidenceInterval(0.95) == null);
+        assertTrue(persistent.getConfidenceInterval(0.95, Tally.LEFT_SIDE_CONFIDENCE) == null);
+
+        // now we initialize the persistent
+
+        persistent.initialize();
+
+        // now we check wether all the properties are correct
+        assertTrue(persistent.getMin() == Double.MAX_VALUE);
+        assertTrue(persistent.getMax() == -Double.MAX_VALUE);
+        assertTrue(Double.valueOf(persistent.getSampleMean()).isNaN());
+        assertTrue(Double.valueOf(persistent.getSampleVariance()).isNaN());
+        assertTrue(Double.valueOf(persistent.getStdDev()).isNaN());
+        assertTrue(persistent.getSum() == 0);
+        assertTrue(persistent.getN() == 0);
+        assertTrue(persistent.getConfidenceInterval(0.95) == null);
+        assertTrue(persistent.getConfidenceInterval(0.95, Tally.LEFT_SIDE_CONFIDENCE) == null);
+
+        // We first fire a wrong event
+        try
+        {
+            persistent.notify(new Event(null, "ERROR", "ERROR"));
+            fail("persistent should react on events.value !instanceOf Double");
+        }
+        catch (Exception exception)
+        {
+            assertNotNull(exception);
+        }
+
+        // Now we fire some events
+        try
+        {
+            persistent.notify(new TimedEvent<Double>(null, "PersistentTest", Double.valueOf(1.0), 0.0));
+            persistent.notify(new TimedEvent<Double>(null, "PersistentTest", Double.valueOf(1.1), 0.1));
+            persistent.notify(new TimedEvent<Double>(null, "PersistentTest", Double.valueOf(1.2), 0.2));
+            persistent.notify(new TimedEvent<Double>(null, "PersistentTest", Double.valueOf(1.3), 0.3));
+            persistent.notify(new TimedEvent<Double>(null, "PersistentTest", Double.valueOf(1.4), 0.4));
+            persistent.notify(new TimedEvent<Double>(null, "PersistentTest", Double.valueOf(1.5), 0.5));
+            persistent.notify(new TimedEvent<Double>(null, "PersistentTest", Double.valueOf(1.6), 0.6));
+            persistent.notify(new TimedEvent<Double>(null, "PersistentTest", Double.valueOf(1.7), 0.7));
+            persistent.notify(new TimedEvent<Double>(null, "PersistentTest", Double.valueOf(1.8), 0.8));
+            persistent.notify(new TimedEvent<Double>(null, "PersistentTest", Double.valueOf(1.9), 0.9));
+            persistent.notify(new TimedEvent<Double>(null, "PersistentTest", Double.valueOf(2.0), 1.0));
+            persistent.notify(new TimedEvent<Double>(null, "PersistentTest", Double.valueOf(2.1), 1.1));
+        }
+        catch (Exception exception)
+        {
+            fail(exception.getMessage());
+        }
+
+        // Now we check the persistent
+        assertTrue(persistent.getMax() == 2.1);
+        assertTrue(persistent.getMin() == 1.0);
+        assertTrue(persistent.getN() == 12);
+        assertTrue(persistent.getSum() == 18.6);
+        assertTrue(Math.abs(persistent.getSampleMean() - 1.5) < 10E-6);
+
+        // Let's compute the standard deviation
+        double variance = 0;
+        for (int i = 0; i < 11; i++)
+        {
+            variance = Math.pow(1.5 - (1.0 + i / 10.0), 2) + variance;
+        }
+        variance = variance / 10.0;
+        double stDev = Math.sqrt(variance);
+
+        assertTrue(Math.abs(persistent.getSampleVariance() - variance) < 10E-6);
+        assertTrue(Math.abs(persistent.getStdDev() - stDev) < 10E-6);
+    }
+}
