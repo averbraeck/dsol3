da76aa6e0441574ec625289072a4421fdc60339b
averbraeck <averbraeck@f713b660-dc99-47c1-a074-784ff2779db6>
2020-05-08 22:17:26 +0000

    deployed dsol version 3.04.05

---
+++ release/3.04.05/dsol-base/src/main/java/nl/tudelft/simulation/language/d3/BoundsUtil.java
@@ -0,0 +1,122 @@
+package nl.tudelft.simulation.language.d3;
+
+import java.awt.geom.Rectangle2D;
+
+import javax.media.j3d.Bounds;
+import javax.media.j3d.Transform3D;
+import javax.vecmath.Point3d;
+import javax.vecmath.Vector3d;
+
+/**
+ * A Bounds utility class to help with finding intersections between bounds, to make transformations, and to see if a point lies
+ * within a bounds. The static methods can help in animation to see whether a shape needs to be drawn on the screen
+ * (3D-viewport) or not.
+ * <p>
+ * Copyright (c) 2002-2020 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
+ * for project information <a href="https://simulation.tudelft.nl/" target="_blank"> https://simulation.tudelft.nl</a>. The DSOL
+ * project is distributed under a three-clause BSD-style license, which can be found at
+ * <a href="https://simulation.tudelft.nl/dsol/3.0/license.html" target="_blank">
+ * https://simulation.tudelft.nl/dsol/3.0/license.html</a>.
+ * </p>
+ * @author <a href="https://www.linkedin.com/in/peterhmjacobs">Peter Jacobs </a>
+ */
+public final class BoundsUtil
+{
+    /**
+     * constructs a new BoundsUtil.
+     */
+    private BoundsUtil()
+    {
+        super();
+        // unreachable code
+    }
+
+    /**
+     * Computes the intersect of the bounding box of the provided bounds with the plane spanned by the zValue. Note that the
+     * bounds are <b>relative to the center</b> and provided without the translation and rotation of the center directed point!
+     * Usually the center is in the bounds, but that is not necessary. The center is in many occasions the Location of an
+     * animated object, and the bounds indicate the outer values of its animation relative to (0,0,0). When the bounding box has
+     * no values on the given z-height, null is returned. As an example: suppose the bounding box has corner coordinates
+     * (-1,-1,-1) and (1,1,1) (all relative to 0,0,0), and the current center location is (0,0,0) and we ask the zIntersect for
+     * z=0, we get a rectangle [(-1,-1), (1,1)]. When we ask the zInterzect for z=2 and center point (2,2,0) we get null (no
+     * bounds on height z=2). When we ask the zIntersect for z=0 and point (2,2,0) we get [(1,1), (3,3)] -- the box is
+     * translated with dx=2 and dy=2. When we ask the zIntersect for z=0 and point (2,2,2) we get null as we 'lifted' the
+     * bounding box above the z-value. When center has a direction, the bounds is first rotated around the center after which
+     * the translation takes place and the bounds on the z-height are calculated.
+     * @param bounds Bounds; the bounds for which the intersection needs to be calculated. The Bounds are <b>relative to the
+     *            center</b> that is provided
+     * @param center DirectedPoint; the point relative to which the bounds need to be calculated
+     * @param zValue double; the zValue as the 'height' for which the bounds intersection is calculated
+     * @return Rectangle2D the resulting rectangle of the intersection
+     */
+    public static Rectangle2D zIntersect(final DirectedPoint center, final Bounds bounds, final double zValue)
+    {
+        BoundingBox box = new BoundingBox((Bounds) bounds.clone());
+        Transform3D transform = new Transform3D();
+        transform.rotZ(center.getRotZ());
+        transform.rotY(center.getRotY());
+        transform.rotX(center.getRotX());
+        transform.setTranslation(new Vector3d(new Point3d(center.x, center.y, center.z)));
+        box.transform(transform);
+
+        Point3d lower = new Point3d();
+        box.getLower(lower);
+        lower.set(lower.x, lower.y, zValue);
+        if (!box.intersect(lower))
+        {
+            return null;
+        }
+        Point3d upper = new Point3d();
+        box.getUpper(upper);
+        return new Rectangle2D.Double(lower.x, lower.y, (upper.x - lower.x), (upper.y - lower.y));
+    }
+
+    /**
+     * Rotates and translates a bound relative to a directed point. Often this point will be the given center point for the
+     * animation.
+     * @param point DirectedPoint; the point relative to which the bounds need to be transformed
+     * @param bounds Bounds; the bounds that need to be rotated and translated
+     * @return the bounds after rotation and translation
+     */
+    public static Bounds transform(final Bounds bounds, final DirectedPoint point)
+    {
+        Bounds result = (Bounds) bounds.clone();
+
+        // First we rotate around 0,0,0
+        Transform3D transform = new Transform3D();
+        transform.rotX(point.getRotX());
+        transform.rotY(point.getRotY());
+        transform.rotZ(point.getRotZ());
+        transform.setTranslation(new Vector3d(point));
+        result.transform(transform);
+        return result;
+    }
+
+    /**
+     * Check whether a point is in the bounds, after transforming the bounds relative to the center point (in animation that is
+     * the location). Usually the center is in the bounds, but that is not necessary. The center is in many occasions the
+     * Location of an animated object, and the bounds indicate the outer values of its animation without translation and
+     * rotation (as if center is 0,0,0) and has no direction (rotX, rotY and rotZ are 0.0).
+     * @param center DirectedPoint; the 'center' of the bounds.
+     * @param bounds Bounds; the bounds relative to 0,0,0
+     * @param point Point3d; the point that might be in or out of the bounds after they have been rotated and translated
+     *            relative to the center.
+     * @return whether or not the point is in the bounds
+     */
+    public static boolean contains(final DirectedPoint center, final Bounds bounds, final Point3d point)
+    {
+        BoundingBox box = new BoundingBox((Bounds) bounds.clone());
+        Transform3D transform = new Transform3D();
+        transform.rotZ(center.getRotZ());
+        transform.rotY(center.getRotY());
+        transform.rotX(center.getRotX());
+        transform.setTranslation(new Vector3d(center));
+        box.transform(transform);
+        Point3d lower = new Point3d();
+        box.getLower(lower);
+        Point3d upper = new Point3d();
+        box.getUpper(upper);
+        return (point.x >= lower.x && point.x <= upper.x && point.y >= lower.y && point.y <= upper.y && point.z >= lower.z
+                && point.z <= upper.z);
+    }
+}
---
+++ release/3.04.05/dsol-base/src/test/java/nl/tudelft/simulation/language/d3/PointTest.java
@@ -0,0 +1,145 @@
+package nl.tudelft.simulation.language.d3;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.awt.geom.Point2D;
+
+import javax.vecmath.Point3d;
+import javax.vecmath.Point3f;
+import javax.vecmath.Tuple3d;
+import javax.vecmath.Tuple3f;
+
+import org.junit.Test;
+
+/**
+ * PointTest.java.
+ * <p>
+ * Copyright (c) 2019-2020 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="https://djunits.org/docs/license.html">DJUNITS License</a>.
+ * <p>
+ * @author <a href="https://www.tudelft.nl/averbraeck" target="_blank">Alexander Verbraeck</a>
+ */
+public class PointTest
+{
+    /**
+     * Test the DirectedPoint (and all constructors of the CartesianPoint).
+     */
+    @Test
+    public void testDirectedPoint()
+    {
+        DirectedPoint p1 = new DirectedPoint();
+        testDP(p1, 0, 0, 0, 0, 0, 0);
+        p1 = new DirectedPoint(1, 2, 3);
+        testDP(p1, 1, 2, 3, 0, 0, 0);
+        p1 = new DirectedPoint(4, 5, 6, .2, .3, .4);
+        testDP(p1, 4, 5, 6, .2, .3, .4);
+        assertTrue(p1.equals(p1.clone()));
+        DirectedPoint p2 = new DirectedPoint(p1);
+        testDP(p2, 4, 5, 6, .2, .3, .4);
+        assertEquals(p1, p2);
+        assertEquals(p1.hashCode(), p2.hashCode());
+        p1 = new DirectedPoint(new double[] {2, 4, 6});
+        testDP(p1, 2, 4, 6, 0, 0, 0);
+        p1 = new DirectedPoint(new Point2D.Double(3.5, 7.0));
+        testDP(p1, 3.5, 7.0, 0, 0, 0, 0);
+        p1 = new DirectedPoint(new Point2D.Double(3.5, 7.0), 0.707);
+        testDP(p1, 3.5, 7.0, 0, 0, 0, 0.707);
+        p1.setX(10.0);
+        p1.setY(11.0);
+        p1.setZ(12.0);
+        p1.setRotX(0.1);
+        p1.setRotY(0.2);
+        p1.setRotZ(0.3);
+        testDP(p1, 10, 11, 12, .1, .2, .3);
+        p1 = new DirectedPoint(new SphericalPoint(1.0, 0.0, 0.0));
+        testDP(p1, 0, 0, 1, 0, 0, 0);
+        p1 = new DirectedPoint(new Point3d(5, 6, 7));
+        testDP(p1, 5, 6, 7, 0, 0, 0);
+        p1 = new DirectedPoint(new Point3f(3f, 4f, 5f));
+        testDP(p1, 3, 4, 5, 0, 0, 0);
+        p1 = new DirectedPoint((Tuple3d) new Point3d(5, 6, 7));
+        testDP(p1, 5, 6, 7, 0, 0, 0);
+        p1 = new DirectedPoint((Tuple3f) new Point3f(3f, 4f, 5f));
+        testDP(p1, 3, 4, 5, 0, 0, 0);
+        assertTrue(p1.equals(p1));
+        assertFalse(p1.equals(p2));
+        assertFalse(p1.equals(new Point3d(3, 4, 5)));
+        assertFalse(p1.equals(new DirectedPoint(3, 4, 5, 0.1, 0, 0)));
+        assertFalse(p1.equals(new DirectedPoint(3, 4, 5, 0, 0.1, 0)));
+        assertFalse(p1.equals(new DirectedPoint(3, 4, 5, 0, 0, 0.1)));
+        p1 = new DirectedPoint(123, 456, 789, 0.135, 0.246, 0.357);
+        assertTrue(p1.toString().contains("123.0"));
+        assertTrue(p1.toString().contains("456.0"));
+        assertTrue(p1.toString().contains("789.0"));
+        assertTrue(p1.toString().contains("RotX=0.135"));
+        assertTrue(p1.toString().contains("RotY=0.246"));
+        assertTrue(p1.toString().contains("RotZ=0.357"));
+    }
+
+    /**
+     * @param p the point to check
+     * @param x double; expected x
+     * @param y double; expected y
+     * @param z double; expected z
+     * @param rX double; expected rotX
+     * @param rY double; expected rotY
+     * @param rZ double; expected rotZ
+     */
+    private void testDP(DirectedPoint p, double x, double y, double z, double rX, double rY, double rZ)
+    {
+        assertNotNull(p);
+        assertEquals(x, p.x, 0.0001);
+        assertEquals(x, p.getX(), 0.0001);
+        assertEquals(y, p.y, 0.0001);
+        assertEquals(y, p.getY(), 0.0001);
+        assertEquals(z, p.z, 0.0001);
+        assertEquals(z, p.getZ(), 0.0001);
+        assertEquals(rX, p.getRotX(), 0.0001);
+        assertEquals(rY, p.getRotY(), 0.0001);
+        assertEquals(rZ, p.getRotZ(), 0.0001);
+    }
+
+    /**
+     * Test the CartesianPoint and the SphericalPoint.
+     */
+    @SuppressWarnings("unlikely-arg-type")
+    @Test
+    public void testCartesianSphericalPoint()
+    {
+        CartesianPoint cp = new CartesianPoint(1.0, 2.0, 3.0);
+        assertTrue(cp.equals(cp));
+        assertEquals(1.0, cp.to2D().getX(), 0.001);
+        assertEquals(2.0, cp.to2D().getY(), 0.001);
+        cp = new CartesianPoint(1, 1, 1);
+        SphericalPoint sp = cp.toSphericalPoint();
+        assertEquals(Math.sqrt(3), sp.getRadius(), 0.001);
+        // theta = arctan y/x
+        assertEquals(Math.atan(1.0), sp.getTheta(), 0.001);
+        // phi = arccos z/r
+        assertEquals(Math.acos(1.0 / Math.sqrt(3)), sp.getPhi(), 0.001);
+        assertTrue(sp.equals(sp));
+        assertFalse(sp.equals(cp));
+        assertFalse(cp.equals(sp));
+        CartesianPoint cp1 = cp.toSphericalPoint().toCartesianPoint();
+        assertEquals(cp.x, cp1.x, 0.001);
+        assertEquals(cp.y, cp1.y, 0.001);
+        assertEquals(cp.z, cp1.z, 0.001);
+        SphericalPoint sp1 = sp.toCartesianPoint().toSphericalPoint();
+        assertEquals(sp.getRadius(), sp1.getRadius(), 0.001);
+        assertEquals(sp.getTheta(), sp1.getTheta(), 0.001);
+        assertEquals(sp.getPhi(), sp1.getPhi(), 0.001);
+        sp = new SphericalPoint(2.5, 0.2, 1.2);
+        assertTrue(sp.equals(new SphericalPoint(2.5, 0.2, 1.2)));
+        assertEquals(sp.hashCode(), new SphericalPoint(2.5, 0.2, 1.2).hashCode());
+        assertFalse(sp.equals(new SphericalPoint(2.0, 0.2, 1.2)));
+        assertFalse(sp.equals(new SphericalPoint(2.5, 0.25, 1.2)));
+        assertFalse(sp.equals(new SphericalPoint(2.5, 0.2, 1.25)));
+        assertFalse(sp.equals(null));
+        assertTrue(sp.toString().contains("radius=2.5"));
+        assertTrue(sp.toString().contains("theta=0.2"));
+        assertTrue(sp.toString().contains("phi=1.2"));
+    }
+}
