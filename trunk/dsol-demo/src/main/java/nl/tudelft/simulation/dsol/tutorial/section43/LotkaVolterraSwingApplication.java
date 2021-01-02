3998cd53c73bc72024fa1c8feae2a69453ef356d
averbraeck <averbraeck@f713b660-dc99-47c1-a074-784ff2779db6>
2021-01-02 02:44:48 +0000

    Major updates in gis, animation, swing for DSOL version 3.05.01. Demos working again. Animation working again. GIS updated.

--- trunk/dsol-demo/src/main/java/nl/tudelft/simulation/dsol/tutorial/section43/LotkaVolterraSwingApplication.java
+++ trunk/dsol-demo/src/main/java/nl/tudelft/simulation/dsol/tutorial/section43/LotkaVolterraSwingApplication.java
@@ -7,11 +7,11 @@ import javax.naming.NamingException;
 import nl.tudelft.simulation.dsol.SimRuntimeException;
 import nl.tudelft.simulation.dsol.experiment.Replication;
 import nl.tudelft.simulation.dsol.experiment.ReplicationMode;
-import nl.tudelft.simulation.dsol.simtime.SimTimeDouble;
 import nl.tudelft.simulation.dsol.simulators.DESSSimulator;
 import nl.tudelft.simulation.dsol.swing.gui.DSOLApplication;
 import nl.tudelft.simulation.dsol.swing.gui.DSOLPanel;
 import nl.tudelft.simulation.dsol.swing.gui.TablePanel;
+import nl.tudelft.simulation.dsol.swing.gui.control.GenericControlPanel;
 
 /**
  * The interactive model for the Lotka-Volterra differential equation.
@@ -28,11 +28,12 @@ public class LotkaVolterraSwingApplication extends DSOLApplication
 {
     /**
      * @param title String; the title
-     * @param panel DSOLPanel&lt;Double,Double,SimTimeDouble&gt;; the panel
+     * @param panel DSOLPanel; the panel
      */
-    public LotkaVolterraSwingApplication(final String title, final DSOLPanel<Double, Double, SimTimeDouble> panel)
+    public LotkaVolterraSwingApplication(final String title, final DSOLPanel panel)
     {
-        super(title, panel);
+        super(panel, title);
+        panel.enableSimulationControlButtons();
     }
 
     /** */
@@ -51,27 +52,31 @@ public class LotkaVolterraSwingApplication extends DSOLApplication
         Replication.TimeDouble<DESSSimulator.TimeDouble> replication =
                 Replication.TimeDouble.create("rep1", 0.0, 0.0, 100.0, model);
         simulator.initialize(replication, ReplicationMode.TERMINATING);
-        new LotkaVolterraSwingApplication("DESS model", new LotkaVolterraPanel(model, simulator));
+
+        GenericControlPanel.TimeDouble controlPanel = new GenericControlPanel.TimeDouble(model, simulator);
+        new LotkaVolterraSwingApplication("DESS model", new LotkaVolterraPanel(model, controlPanel));
     }
 
     /** The panel. */
-    private static class LotkaVolterraPanel extends DSOLPanel<Double, Double, SimTimeDouble>
+    private static class LotkaVolterraPanel extends DSOLPanel
     {
         /** */
         private static final long serialVersionUID = 1L;
 
         /**
          * @param model PredatorPreyModel; the model
-         * @param simulator DESSSimulator.TimeDouble; the simulator
+         * @param controlPanel GenericControlPanel; the control panel for the interactive simulation
+         * @throws RemoteException on error
          */
-        LotkaVolterraPanel(final PredatorPreyModel model, final DESSSimulator.TimeDouble simulator)
+        LotkaVolterraPanel(final PredatorPreyModel model, final GenericControlPanel.TimeDouble controlPanel)
+                throws RemoteException
         {
-            super(model, simulator);
+            super(controlPanel);
 
             // add a chart for the demo.
             TablePanel charts = new TablePanel(1, 1);
             super.tabbedPane.addTab("statistics", charts);
-            super.tabbedPane.setSelectedIndex(1);
+            super.tabbedPane.setSelectedIndex(0);
             charts.setCell(model.getChart().getSwingPanel(), 0, 0);
         }
     }
