7b0fc21aac66091416fd53ced75029e8f2f84388
averbraeck <averbraeck@f713b660-dc99-47c1-a074-784ff2779db6>
2021-01-31 13:34:32 +0000

    Committed version 3.05.04

---
+++ release/3.05.04/dsol-demo/src/main/java/nl/tudelft/simulation/examples/dsol/animation/BallSwingApplication.java
@@ -0,0 +1,66 @@
+package nl.tudelft.simulation.examples.dsol.animation;
+
+import java.rmi.RemoteException;
+
+import org.djutils.draw.bounds.Bounds2d;
+import org.pmw.tinylog.Level;
+
+import nl.tudelft.simulation.dsol.animation.D2.RenderableScale;
+import nl.tudelft.simulation.dsol.experiment.Replication;
+import nl.tudelft.simulation.dsol.experiment.ReplicationMode;
+import nl.tudelft.simulation.dsol.simulators.DEVSRealTimeAnimator;
+import nl.tudelft.simulation.dsol.simulators.DEVSSimulatorInterface;
+import nl.tudelft.simulation.dsol.swing.gui.ConsoleLogger;
+import nl.tudelft.simulation.dsol.swing.gui.ConsoleOutput;
+import nl.tudelft.simulation.dsol.swing.gui.DSOLPanel;
+import nl.tudelft.simulation.dsol.swing.gui.animation.DSOLAnimationApplication;
+import nl.tudelft.simulation.dsol.swing.gui.control.RealTimeControlPanel;
+import nl.tudelft.simulation.language.DSOLException;
+
+/**
+ * <p>
+ * Copyright (c) 2002-2021 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
+ * for project information <a href="https://simulation.tudelft.nl/" target="_blank"> https://simulation.tudelft.nl</a>. The DSOL
+ * project is distributed under a three-clause BSD-style license, which can be found at
+ * <a href="https://simulation.tudelft.nl/dsol/3.0/license.html" target="_blank">
+ * https://simulation.tudelft.nl/dsol/3.0/license.html</a>.
+ * </p>
+ * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ */
+public class BallSwingApplication extends DSOLAnimationApplication
+{
+    /**
+     * @param title String; the title
+     * @param panel DSOLPanel; the panel
+     * @throws DSOLException when simulator is not an animator
+     * @throws IllegalArgumentException for illegal bounds
+     * @throws RemoteException on network error
+     */
+    public BallSwingApplication(final String title, final DSOLPanel panel)
+            throws RemoteException, IllegalArgumentException, DSOLException
+    {
+        super(panel, title, new Bounds2d(-100, 100, -100, 100));
+        getAnimationTab().getAnimationPanel().setRenderableScale(new RenderableScale(2.0));
+        panel.enableSimulationControlButtons();
+    }
+
+    /** */
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @param args String[]; arguments, expected to be empty
+     * @throws Exception on error
+     */
+    public static void main(final String[] args) throws Exception
+    {
+        DEVSRealTimeAnimator.TimeDouble simulator = new DEVSRealTimeAnimator.TimeDouble("BallSwingApplication", 0.001);
+        BallModel model = new BallModel(simulator);
+        Replication.TimeDouble<DEVSSimulatorInterface.TimeDouble> replication =
+                Replication.TimeDouble.create("rep1", 0.0, 0.0, 1000000.0, model);
+        simulator.initialize(replication, ReplicationMode.TERMINATING);
+        DSOLPanel panel = new DSOLPanel(new RealTimeControlPanel.TimeDouble(model, simulator));
+        panel.addTab("logger", new ConsoleLogger(Level.INFO));
+        panel.addTab("console", new ConsoleOutput());
+        new BallSwingApplication("BallSwingApplication", panel);
+    }
+}
