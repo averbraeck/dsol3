45cfa5a5103af1eaf2917cc9df8346e587702ee0
averbraeck <averbraeck@f713b660-dc99-47c1-a074-784ff2779db6>
2021-02-20 14:43:06 +0000

    Committed version 3.06.01

---
+++ release/3.06.01/dsol-animation/src/main/java/nl/tudelft/simulation/dsol/animation/gis/MapUnits.java
@@ -0,0 +1,33 @@
+package nl.tudelft.simulation.dsol.animation.gis;
+
+/**
+ * MapUnits indicates the units for a map.
+ * <p>
+ * Copyright (c) 2020-2021 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
+ * for project information <a href="https://simulation.tudelft.nl/dsol/manual/" target="_blank">DSOL Manual</a>. The DSOL
+ * project is distributed under a three-clause BSD-style license, which can be found at
+ * <a href="https://simulation.tudelft.nl/dsol/3.0/license.html" target="_blank">DSOL License</a>.
+ * </p>
+ * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ */
+public enum MapUnits
+{
+    /** FEET constant. */
+    FEET,
+
+    /** INCHES constant. */
+    INCHES,
+
+    /** KILOMETERS constant. */
+    KILOMETERS,
+
+    /** METERS constant. */
+    METERS,
+
+    /** MILES constant. */
+    MILES,
+
+    /** DECIMAL DEGREES constant. A decimal degree is 1/100 of the earth circumference. */
+    DECIMAL_DEGREES;
+
+}
