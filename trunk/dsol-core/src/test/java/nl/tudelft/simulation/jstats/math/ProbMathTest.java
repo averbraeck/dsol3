e97735c37e879cfad9a22988aa6ffccbecd6d9dc
averbraeck <averbraeck@f713b660-dc99-47c1-a074-784ff2779db6>
2021-03-09 23:27:59 +0000

    Code for permutations has been fixed in ProbMath class (the code calculated combinations). Code for combinations added to ProbMath class. Code for error function erf(z) added to ProbMath class, using three different Taylor expansions, which are used depending on the interval of the argument. For most of the interval, the error is less than 1E-10, with a relative error cutoff for the Taylor expansions of 1E-16. Code for inverse error function erfInv(z) added to ProbMath class, using three different approximations, which are used depending on the interval of the argument. For most of the interval, the error is less than 1E-9. The Normal cumulative probability and inverse cumulative probability functions now use erf() and erfInv() functions instead of a lookup table, and thereby became much more precise.

--- trunk/dsol-core/src/test/java/nl/tudelft/simulation/jstats/math/ProbMathTest.java
+++ trunk/dsol-core/src/test/java/nl/tudelft/simulation/jstats/math/ProbMathTest.java
@@ -2,60 +2,153 @@ package nl.tudelft.simulation.jstats.math;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
+import org.djutils.exceptions.Try;
 import org.junit.Test;
 
 /**
- * The test script for the ProbMath class.
+ * The tests for the ProbMath class.
  * <p>
- * Copyright (c) 2002-2021 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
+ * Copyright (c) 2021-2021 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
  * for project information <a href="https://simulation.tudelft.nl/" target="_blank"> https://simulation.tudelft.nl</a>. The DSOL
  * project is distributed under a three-clause BSD-style license, which can be found at
  * <a href="https://simulation.tudelft.nl/dsol/3.0/license.html" target="_blank">
  * https://simulation.tudelft.nl/dsol/3.0/license.html</a>.
  * </p>
- * @author <a href="https://www.linkedin.com/in/peterhmjacobs">Peter Jacobs </a>
- * @since 1.5
+ * @author <a href="https://www.tudelft.nl/averbraeck" target="_blank"> Alexander Verbraeck</a>
  */
 public class ProbMathTest
 {
     /**
-     * tests the probMath class.
+     * Test the factorial functions.
      */
     @Test
-    public void testProbMath()
+    public void testFactorial()
     {
-        // First the faculty function
-        try
-        {
-            ProbMath.faculty(-1);
-            fail();
-        }
-        catch (Exception exception)
-        {
-            assertEquals(exception.getClass(), IllegalArgumentException.class);
-        }
-        assertTrue(ProbMath.faculty(0) == 1.0);
-        assertTrue(ProbMath.faculty(10) == 3628800.0);
-        try
-        {
-            ProbMath.faculty(171);
-            fail();
-        }
-        catch (Exception exception)
-        {
-            assertEquals(exception.getClass(), IllegalArgumentException.class);
-        }
+        assertEquals(1.0, ProbMath.factorial(0), 0.00001);
+        assertEquals(3628800.0, ProbMath.factorial(10), 0.00001);
+        assertEquals(1L, ProbMath.fac(0));
+        assertEquals(3628800L, ProbMath.fac(10));
 
-        // Permutations
-        try
+        assertTrue(ProbMath.factorial(170) > 0.0);
+        assertTrue(ProbMath.fac(20) > 0L);
+
+        Try.testFail(new Try.Execution()
         {
-            ProbMath.permutations(2, 5);
-        }
-        catch (Exception exception)
+            @Override
+            public void execute() throws Throwable
+            {
+                ProbMath.factorial(-1);
+            }
+        }, "factorial(-1)", IllegalArgumentException.class);
+        Try.testFail(new Try.Execution()
         {
-            assertEquals(exception.getClass(), IllegalArgumentException.class);
+            @Override
+            public void execute() throws Throwable
+            {
+                ProbMath.fac(-1);
+            }
+        }, "fac(-1)", IllegalArgumentException.class);
+        Try.testFail(new Try.Execution()
+        {
+            @Override
+            public void execute() throws Throwable
+            {
+                ProbMath.factorial(171);
+            }
+        }, "factorial(171)", IllegalArgumentException.class);
+        Try.testFail(new Try.Execution()
+        {
+            @Override
+            public void execute() throws Throwable
+            {
+                ProbMath.fac(21);
+            }
+        }, "fac(21)", IllegalArgumentException.class);
+    }
+
+    /**
+     * Test the permutations functions.
+     */
+    @Test
+    public void testPermutation()
+    {
+        assertEquals(336.0, ProbMath.permutations(8, 3), 0.0001);
+        assertEquals(336L, ProbMath.perm(8, 3));
+
+        Try.testFail(new Try.Execution()
+        {
+            @Override
+            public void execute() throws Throwable
+            {
+                ProbMath.permutations(2, 5);
+            }
+        }, "permutations(2, 5)", IllegalArgumentException.class);
+        Try.testFail(new Try.Execution()
+        {
+            @Override
+            public void execute() throws Throwable
+            {
+                ProbMath.perm(2, 5);
+            }
+        }, "perm(2, 5)", IllegalArgumentException.class);
+    }
+
+    /**
+     * Test the combination functions.
+     */
+    @Test
+    public void testCombination()
+    {
+        assertEquals(56.0, ProbMath.combinations(8, 3), 0.0001);
+        assertEquals(56L, ProbMath.comb(8, 3));
+
+        Try.testFail(new Try.Execution()
+        {
+            @Override
+            public void execute() throws Throwable
+            {
+                ProbMath.combinations(2, 5);
+            }
+        }, "combinations(2, 5)", IllegalArgumentException.class);
+        Try.testFail(new Try.Execution()
+        {
+            @Override
+            public void execute() throws Throwable
+            {
+                ProbMath.comb(2, 5);
+            }
+        }, "comb(2, 5)", IllegalArgumentException.class);
+    }
+
+    /**
+     * Test the erf(z)and erf2(z) functions.
+     */
+    @Test
+    public void testErf()
+    {
+        assertEquals(0.0, ProbMath.erf(0.0), 0.0001);
+        assertEquals(0.52049987781, ProbMath.erf(0.5), 0.0001);
+        assertEquals(-0.52049987781, ProbMath.erf(-0.5), 0.0001);
+        assertEquals(0.84270079295, ProbMath.erf(1.0), 0.0001);
+        assertEquals(-0.84270079295, ProbMath.erf(-1.0), 0.0001);
+        assertEquals(1.0, ProbMath.erf(3.0), 0.0001);
+        assertEquals(1.0, ProbMath.erf(4.0), 0.0001);
+        assertEquals(-1.0, ProbMath.erf(-4.0), 0.0001);
+        assertEquals(0.47693672, ProbMath.erfInv(0.5), 0.0001);
+
+        double[] testWikipedia = new double[] {0.02, 0.022564575, 0.04, 0.045111106, 0.06, 0.067621594, 0.08, 0.090078126, 0.1,
+                0.112462916, 0.2, 0.222702589, 0.3, 0.328626759, 0.4, 0.428392355, 0.5, 0.520499878, 0.6, 0.603856091, 0.7,
+                0.677801194, 0.8, 0.742100965, 0.9, 0.796908212, 1.0, 0.842700793, 1.1, 0.88020507, 1.2, 0.910313978, 1.3,
+                0.934007945, 1.4, 0.95228512, 1.5, 0.966105146, 1.6, 0.976348383, 1.7, 0.983790459, 1.8, 0.989090502, 1.9,
+                0.992790429, 2, 0.995322265, 2.1, 0.997020533, 2.2, 0.998137154, 2.3, 0.998856823, 2.4, 0.999311486, 2.5,
+                0.999593048};
+        for (int i = 0; i < testWikipedia.length / 2; i++)
+        {
+            assertEquals("erf for value " + testWikipedia[2 * i], testWikipedia[2 * i + 1], ProbMath.erf(testWikipedia[2 * i]),
+                    0.0001);
+            assertEquals("erfInv(" + testWikipedia[2 * i + 1] + ") for value " + testWikipedia[2 * i], testWikipedia[2 * i],
+                    ProbMath.erfInv(testWikipedia[2 * i + 1]), 0.0001);
         }
     }
 }
