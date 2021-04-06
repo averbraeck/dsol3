eb6d2d0050ea76609f6f53489d9338723ebfd7b7
averbraeck <averbraeck@f713b660-dc99-47c1-a074-784ff2779db6>
2021-04-06 01:14:31 +0000

    Some further optimizations have been made to the EmpiricalDistribution. One is how to properly transform density functions to a cumulative distribution that uses interpolation.  Unit tests for the EmpiricalDistribution have been expanded.

--- trunk/dsol-demo/src/main/java/nl/tudelft/simulation/dsol/tutorial/section42/Customer.java
+++ trunk/dsol-demo/src/main/java/nl/tudelft/simulation/dsol/tutorial/section42/Customer.java
@@ -6,9 +6,10 @@ import nl.tudelft.simulation.dsol.simtime.SimTimeDouble;
 import nl.tudelft.simulation.dsol.simulators.DEVSSimulatorInterface;
 import nl.tudelft.simulation.jstats.distributions.DistContinuous;
 import nl.tudelft.simulation.jstats.distributions.DistDiscrete;
-import nl.tudelft.simulation.jstats.distributions.DistDiscreteEmpirical;
+import nl.tudelft.simulation.jstats.distributions.DistEmpiricalDiscreteLong;
 import nl.tudelft.simulation.jstats.distributions.DistExponential;
-import nl.tudelft.simulation.jstats.distributions.empirical.EmpiricalDistribution;
+import nl.tudelft.simulation.jstats.distributions.empirical.DiscreteEmpiricalDistribution;
+import nl.tudelft.simulation.jstats.distributions.empirical.ProbabilityDensities;
 import nl.tudelft.simulation.jstats.streams.StreamInterface;
 
 /**
@@ -49,9 +50,9 @@ public class Customer implements BuyerInterface
         this.retailer = retailer;
         StreamInterface stream = this.simulator.getModel().getStream("default");
         this.intervalTime = new DistExponential(stream, 0.1);
-        EmpiricalDistribution empDist = EmpiricalDistribution.createFromDensities(new long[] {1, 2, 3, 4},
-                new double[] {1.0 / 6.0, 1.0 / 3.0, 1.0 / 3.0, 1.0 / 6.0}, false);
-        this.orderBatchSize = new DistDiscreteEmpirical(stream, empDist);
+        DiscreteEmpiricalDistribution empDist = ProbabilityDensities.createDiscreteDistribution(new long[] {1, 2, 3, 4},
+                new double[] {1.0 / 6.0, 1.0 / 3.0, 1.0 / 3.0, 1.0 / 6.0});
+        this.orderBatchSize = new DistEmpiricalDiscreteLong(stream, empDist);
         this.createOrder();
     }
 
