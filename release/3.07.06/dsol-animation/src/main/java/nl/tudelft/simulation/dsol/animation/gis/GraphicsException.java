30c5e6035e9967f2fb9cf6f1d716aaa88f0d2813
averbraeck <averbraeck@f713b660-dc99-47c1-a074-784ff2779db6>
2022-01-22 11:34:59 +0000

    deployed version 3.07.07

---
+++ release/3.07.06/dsol-animation/src/main/java/nl/tudelft/simulation/dsol/animation/gis/GraphicsException.java
@@ -0,0 +1,54 @@
+package nl.tudelft.simulation.dsol.animation.gis;
+
+/**
+ * GraphicsException for exceptions when reading or drawing GIS layers.
+ * <p>
+ * Copyright (c) 2020-2022 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
+ * for project information <a href="https://simulation.tudelft.nl/dsol/manual/" target="_blank">DSOL Manual</a>. The DSOL
+ * project is distributed under a three-clause BSD-style license, which can be found at
+ * <a href="https://simulation.tudelft.nl/dsol/3.0/license.html" target="_blank">DSOL License</a>.
+ * </p>
+ * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ */
+public class GraphicsException extends Exception
+{
+    /** */
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Constructs a new blank GraphicsException.
+     */
+    public GraphicsException()
+    {
+        super();
+    }
+
+    /**
+     * Constructs a new GraphicsException.
+     * @param message String; the message to display
+     * @param cause Throwable; the underlying exception
+     */
+    public GraphicsException(final String message, final Throwable cause)
+    {
+        super(message, cause);
+    }
+
+    /**
+     * Constructs a new GraphicsException.
+     * @param message String; the message to display
+     */
+    public GraphicsException(final String message)
+    {
+        super(message);
+    }
+
+    /**
+     * Constructs a new GraphicsException.
+     * @param cause Throwable; the underlying exception
+     */
+    public GraphicsException(final Throwable cause)
+    {
+        super(cause);
+    }
+
+}
