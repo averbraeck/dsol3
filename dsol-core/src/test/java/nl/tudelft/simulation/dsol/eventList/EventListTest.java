3736e78a76dd3bbd37d9898bdcf05780f187a1b3
Alexander Verbraeck <averbraeck@users.noreply.github.com>
2022-06-29 15:38:43 +0000

    Adapted tests for EventList to match interface changes. Added performance tests for EventList implementations.

--- dsol-core/src/test/java/nl/tudelft/simulation/dsol/eventList/EventListTest.java
+++ dsol-core/src/test/java/nl/tudelft/simulation/dsol/eventList/EventListTest.java
@@ -1,14 +1,15 @@
 package nl.tudelft.simulation.dsol.eventList;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
 import nl.tudelft.simulation.dsol.eventlists.EventListInterface;
+import nl.tudelft.simulation.dsol.eventlists.EventListPriorityQueue;
 import nl.tudelft.simulation.dsol.eventlists.RedBlackTree;
 import nl.tudelft.simulation.dsol.formalisms.eventscheduling.SimEvent;
 import nl.tudelft.simulation.dsol.formalisms.eventscheduling.SimEventInterface;
@@ -28,91 +29,91 @@ import nl.tudelft.simulation.dsol.simtime.SimTimeDouble;
  */
 public class EventListTest
 {
-    /** eventList is the eventList on which the test is fired. */
-    private EventListInterface<SimTimeDouble> eventList = null;
-
     /**
-     * constructs a new RedblackTree.
+     * test the different event list implementations.
      */
-    public EventListTest()
+    @Test
+    public void testAllEventLists()
     {
-        this.eventList = new RedBlackTree<SimTimeDouble>();
+        testEventList(new RedBlackTree<>());
+        testEventList(new EventListPriorityQueue<>());
     }
 
     /**
-     * tests the TreeMapEventListOld.
+     * Test an implementation of the EventList.
+     * @param eventList EventListInterface; the event list to test
      */
-    @Test
-    public void testEventList()
+    public void testEventList(final EventListInterface<SimTimeDouble> eventList)
     {
-        assertNotNull(this.eventList);
-        try
+        assertNotNull(eventList);
+
+        // We fill the eventList with 500 events with random times
+        // between [0..200]
+        for (int i = 0; i < 500; i++)
         {
-            // We fill the eventList with 500 events with random times
-            // between [0..200]
-            for (int i = 0; i < 500; i++)
-            {
-                this.eventList.add(
-                        new SimEvent<SimTimeDouble>(new SimTimeDouble(200 * Math.random()), this, new String(), "trim", null));
-            }
-
-            // Now we assert some getters on the eventList
-            assertTrue(!this.eventList.isEmpty());
-            assertTrue(this.eventList.size() == 500);
-
-            // Let's see if the eventList was properly ordered
-            double time = 0;
-            for (int i = 0; i < 500; i++)
-            {
-                SimEventInterface<SimTimeDouble> simEvent = this.eventList.first();
-                this.eventList.remove(this.eventList.first());
-                double executionTime = simEvent.getAbsoluteExecutionTime().get().doubleValue();
-                assertTrue(executionTime >= 0.0);
-                assertTrue(executionTime <= 200.0);
-                assertTrue(executionTime >= time);
-                time = executionTime;
-            }
-
-            // Now we fill the eventList with a number of events with different priorities on time=0.0
-            for (int i = 1; i < 10; i++)
-            {
-                this.eventList
-                        .add(new SimEvent<SimTimeDouble>(new SimTimeDouble(0.0), (short) i, this, new String(), "trim", null));
-            }
-            short priority = SimEventInterface.MAX_PRIORITY;
-
-            // Let's empty the eventList and check the priorities
-            while (!this.eventList.isEmpty())
-            {
-                SimEventInterface<SimTimeDouble> simEvent = this.eventList.first();
-                this.eventList.remove(this.eventList.first());
-                double executionTime = simEvent.getAbsoluteExecutionTime().get().doubleValue();
-                short eventPriority = simEvent.getPriority();
-
-                assertTrue(executionTime == 0.0);
-                assertTrue(eventPriority <= SimEventInterface.MAX_PRIORITY);
-                assertTrue(eventPriority >= SimEventInterface.MIN_PRIORITY);
-                assertTrue(eventPriority <= priority);
-                priority = eventPriority;
-            }
-
-            // Let's check the empty eventList
-            assertTrue(this.eventList.isEmpty());
-            assertNull(this.eventList.first());
-            assertFalse(this.eventList.remove(
-                    new SimEvent<SimTimeDouble>(new SimTimeDouble(200 * Math.random()), this, new String(), "trim", null)));
-            this.eventList.clear();
-
-            // Let's cancel an event
-            this.eventList.add(new SimEvent<SimTimeDouble>(new SimTimeDouble(100), this, this, "toString", null));
-            SimEventInterface<SimTimeDouble> simEvent = new SimEvent<>(new SimTimeDouble(100), this, this, "toString", null);
-            this.eventList.add(simEvent);
-            assertTrue(this.eventList.remove(simEvent));
+            eventList
+                    .add(new SimEvent<SimTimeDouble>(new SimTimeDouble(200 * Math.random()), this, new String(), "trim", null));
         }
-        catch (Exception exception)
+
+        // Now we assert some getters on the eventList
+        assertTrue(!eventList.isEmpty());
+        assertTrue(eventList.size() == 500);
+
+        // Let's see if the eventList was properly ordered
+        double time = 0;
+        for (int i = 0; i < 500; i++)
         {
-            exception.printStackTrace();
-            fail(exception.getMessage());
+            SimEventInterface<SimTimeDouble> simEvent = eventList.first();
+            eventList.remove(eventList.first());
+            double executionTime = simEvent.getAbsoluteExecutionTime().get().doubleValue();
+            assertTrue(executionTime >= 0.0);
+            assertTrue(executionTime <= 200.0);
+            assertTrue(executionTime >= time);
+            time = executionTime;
         }
+
+        // Now we fill the eventList with a number of events with different priorities on time=0.0
+        for (int i = 1; i < 10; i++)
+        {
+            eventList.add(new SimEvent<SimTimeDouble>(new SimTimeDouble(0.0), (short) i, this, new String(), "trim", null));
+        }
+        short priority = SimEventInterface.MAX_PRIORITY;
+
+        // Let's empty the eventList and check the priorities
+        while (!eventList.isEmpty())
+        {
+            SimEventInterface<SimTimeDouble> simEvent = eventList.first();
+            eventList.remove(eventList.first());
+            double executionTime = simEvent.getAbsoluteExecutionTime().get().doubleValue();
+            short eventPriority = simEvent.getPriority();
+
+            assertTrue(executionTime == 0.0);
+            assertTrue(eventPriority <= SimEventInterface.MAX_PRIORITY);
+            assertTrue(eventPriority >= SimEventInterface.MIN_PRIORITY);
+            assertTrue(eventPriority <= priority);
+            priority = eventPriority;
+        }
+
+        // Let's check the empty eventList
+        assertTrue(eventList.isEmpty());
+        assertNull(eventList.first());
+        assertFalse(eventList
+                .remove(new SimEvent<SimTimeDouble>(new SimTimeDouble(200 * Math.random()), this, new String(), "trim", null)));
+        eventList.clear();
+
+        // Let's cancel an event
+        eventList.add(new SimEvent<SimTimeDouble>(new SimTimeDouble(100), this, this, "toString", null));
+        SimEventInterface<SimTimeDouble> simEvent = new SimEvent<>(new SimTimeDouble(100), this, this, "toString", null);
+        eventList.add(simEvent);
+        assertTrue(eventList.remove(simEvent));
+
+        // test the iterator and removeFirst
+        eventList.clear();
+        eventList.add(simEvent);
+        assertEquals(eventList.first(), eventList.iterator().next());
+        assertEquals(simEvent, eventList.removeFirst());
+        assertTrue(eventList.isEmpty());
+        assertFalse(eventList.contains(simEvent));
+        assertNull(eventList.removeFirst());
     }
 }
